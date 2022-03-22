92c7a9fd4a4fc90447a9f52acd7feb6bd7d07090
Michael <michaelritter@gmx.net>
2022-03-22 09:11:10 +0100

    Enums for stock exchange and price alert type added.

---
+++ src/main/java/backend/model/PriceAlertType.java
@@ -0,0 +1,18 @@
+package backend.model;
+
+/**
+ * The type of a price alert.
+ * 
+ * @author Michael
+ */
+public enum PriceAlertType {
+	/**
+	 * Alert if price is less or equal.
+	 */
+	LESS_OR_EQUAL,
+	
+	/**
+	 * Alert if price is greater or equal.
+	 */
+	GREATER_OR_EQUAL
+}
---
+++ src/main/java/backend/model/StockExchange.java
@@ -0,0 +1,23 @@
+package backend.model;
+
+/**
+ * The exchange at which a stock is listed.
+ * 
+ * @author Michael
+ */
+public enum StockExchange {
+	/**
+	 * Toronto Stock Exchange
+	 */
+	TSX,
+	
+	/**
+	 * Toronto Stock Exchange - Venture
+	 */
+	TSXV,
+	
+	/**
+	 * New York Stock Exchange
+	 */
+	NYSE
+}
