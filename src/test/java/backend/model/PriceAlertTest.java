580747d75a6d2b33ff1678d684794ad8183091f3
Michael <michaelritter@gmx.net>
2022-03-25 08:09:15 +0100

    Add JUnit5 dependency and set up a dummy test.

---
+++ src/test/java/backend/model/PriceAlertTest.java
@@ -0,0 +1,25 @@
+package backend.model;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Tests the price alert model.
+ * 
+ * @author Michael
+ */
+public class PriceAlertTest {
+	@Test
+	/**
+	 * A dummy test verifying that the framework executes tests.
+	 */
+	public void dummyTest() {
+		int a, b;
+		
+		a = 1;
+		b = 1;
+		
+		assertTrue(a == b);
+	}
+}
