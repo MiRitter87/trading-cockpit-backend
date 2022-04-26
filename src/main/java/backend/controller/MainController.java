031f368999d3ceaf9fb8aab30d9458e75c7fc868
Michael <michaelritter@gmx.net>
2022-04-26 09:40:32 +0200

    MainController added.

---
+++ src/main/java/backend/controller/MainController.java
@@ -0,0 +1,66 @@
+package backend.controller;
+
+import java.io.IOException;
+import java.util.ResourceBundle;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import backend.dao.DAOManager;
+
+
+/**
+ * The main controller of the web application.
+ * 
+ * @author Michael
+ */
+public class MainController {
+	/**
+	 * Instance of the main controller.
+	 */
+	private static MainController instance;
+	
+	/**
+	 * Application logging.
+	 */
+	public static final Logger logger = LogManager.getLogger(MainController.class);
+	
+	
+	/**
+	 * Creates and initializes the main controller.
+	 */
+	private MainController() {
+
+	}
+	
+	/**
+	 * Provides the instance of the MainController.
+	 * 
+	 * @return The instance of the MainController.
+	 */
+	public static MainController getInstance() {
+		if(instance == null)
+			instance = new MainController();
+		
+		return instance;
+	}
+	
+	
+	/**
+	 * Performs tasks on application startup.
+	 */
+	public void applicationStartup() {
+		DAOManager.getInstance();
+	}
+
+	/**
+	 * Performs tasks on application shutdown.
+	 */
+	public void applicationShutdown() {
+		try {
+			DAOManager.getInstance().close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
