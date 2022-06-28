776210d70fd4f7a729c9978edb61cd8fb2d7dbd9
Michael <michaelritter@gmx.net>
2022-06-28 08:07:46 +0200

    DAO interface for scan persistence added.

---
+++ src/main/java/backend/dao/scan/ScanDAO.java
@@ -0,0 +1,59 @@
+package backend.dao.scan;
+
+import java.util.List;
+
+import backend.dao.ObjectUnchangedException;
+import backend.model.scan.Scan;
+
+/**
+ * Interface for scan persistence.
+ * 
+ * @author Michael
+ */
+public interface ScanDAO {
+	/**
+	 * Inserts a scan.
+	 * 
+	 * @param scan The scan to be inserted.
+	 * @throws Exception Insertion failed.
+	 */
+	void insertScan(final Scan scan) throws Exception;
+	
+	
+	/**
+	 * Deletes a scan.
+	 * 
+	 * @param scan The scan to be deleted.
+	 * @throws Exception Deletion failed.
+	 */
+	void deleteScan(final Scan scan) throws Exception;
+	
+	
+	/**
+	 * Gets all scans.
+	 * 
+	 * @return All scans.
+	 * @throws Exception List retrieval failed.
+	 */
+	List<Scan> getScans() throws Exception;
+	
+	
+	/**
+	 * Gets the scan with the given id.
+	 * 
+	 * @param id The id of the scan.
+	 * @return The scan with the given id.
+	 * @throws Exception Scan retrieval failed.
+	 */
+	Scan getScan(final Integer id) throws Exception;
+	
+	
+	/**
+	 * Updates the given scan.
+	 * 
+	 * @param scan The scan to be updated.
+	 * @throws ObjectUnchangedException In case the data of the scan have not been changed.
+	 * @throws Exception Scan update failed.
+	 */
+	void updateScan(final Scan scan) throws ObjectUnchangedException, Exception;
+}
