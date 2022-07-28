901c7c1da7567e7d523369a3d75fa6bbd5c86b55
MiRitter87 <michaelritter@gmx.net>
2022-07-28 13:45:53 +0200

    New method to calculate the percentage sum of an instruments quotation.

--- src/main/java/backend/controller/scan/IndicatorCalculator.java
+++ src/main/java/backend/controller/scan/IndicatorCalculator.java
@@ -1,10 +1,65 @@
 package backend.controller.scan;
 
+import java.math.BigDecimal;
+import java.math.MathContext;
+import java.math.RoundingMode;
+import java.util.List;
+
+import backend.model.instrument.Instrument;
+import backend.model.instrument.Quotation;
+
 /**
  * Performs calculations of indicators based on the instruments quotations.
  * 
  * @author Michael
  */
 public class IndicatorCalculator {
+	/**
+	 * The number of trading days per month.
+	 */
+	private static final int TRADING_DAYS_PER_MONTH = 21;
 	
+	/**
+	 * Calculates the percentage sum needed for calculation of the RS number.
+	 * 
+	 * @param instrument The instrument whose quotations are used for calculation.
+	 * @param quotation The quotation of the date on which the percentage sum is calculated.
+	 * @return The percentage sum.
+	 */
+	public float getRSPercentSum(final Instrument instrument, final Quotation quotation) {
+		List<Quotation> sortedQuotations = instrument.getQuotationsSortedByDate();
+		int indexOfQuotation = sortedQuotations.indexOf(quotation);
+		BigDecimal rsPercentSum = BigDecimal.valueOf(0);
+		
+		rsPercentSum = rsPercentSum.add(this.getPerformanceOfInterval(sortedQuotations, indexOfQuotation, 3));
+		rsPercentSum = rsPercentSum.add(this.getPerformanceOfInterval(sortedQuotations, indexOfQuotation, 3));
+		rsPercentSum = rsPercentSum.add(this.getPerformanceOfInterval(sortedQuotations, indexOfQuotation, 6));
+		rsPercentSum = rsPercentSum.add(this.getPerformanceOfInterval(sortedQuotations, indexOfQuotation, 9));
+		rsPercentSum = rsPercentSum.add(this.getPerformanceOfInterval(sortedQuotations, indexOfQuotation, 12));
+		
+		rsPercentSum.setScale(2);
+		
+		return rsPercentSum.floatValue();
+	}
+	
+	
+	/**
+	 * Provides the performance of a given interval.
+	 * 
+	 * @param sortedQuotations The quotations containing date and price information for performance calculation.
+	 * @param indexOfQuotation The starting point from which the performance is calculated.
+	 * @param months The number of months for performance calculation.
+	 * @return The performance of the given interval in percent.
+	 */
+	private BigDecimal getPerformanceOfInterval(final List<Quotation> sortedQuotations, final int indexOfQuotation, final int months) {
+		BigDecimal divisionResult = BigDecimal.valueOf(0);
+		int indexOfQuotationForInterval = indexOfQuotation + (TRADING_DAYS_PER_MONTH * months) -1;
+		
+		divisionResult = sortedQuotations.get(indexOfQuotation).getPrice().divide
+				(sortedQuotations.get(indexOfQuotationForInterval).getPrice(), 4, RoundingMode.HALF_UP);
+		divisionResult = divisionResult.subtract(BigDecimal.valueOf(1));
+		divisionResult = divisionResult.multiply(BigDecimal.valueOf(100));
+		
+		return divisionResult;
+	}
 }
--- src/test/java/backend/controller/scan/IndicatorCalculatorTest.java
+++ src/test/java/backend/controller/scan/IndicatorCalculatorTest.java
@@ -1,14 +1,17 @@
 package backend.controller.scan;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.fail;
 
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import backend.dao.instrument.QuotationYahooDAO;
 import backend.dao.instrument.QuotationYahooDAOStub;
@@ -103,5 +106,28 @@ public class IndicatorCalculatorTest {
 		} catch (Exception e) {
 			fail(e.getMessage());
 		}
+	}
+	
+	
+	@Test
+	/**
+	 * Tests the calculation of the percentage sum.
+	 */
+	public void testGetRSPercentSum() {
+		List<Quotation> sortedQuotations = this.dmlStock.getQuotationsSortedByDate();
+		float expectedRSPercentSum, actualRSPercentSum;
+		
+		//Actual price: 1,36
+		//Three months ago price: 1,75
+		//Six months ago price: 1,49
+		//Nine months ago price: 2,30
+		//Twelve months ago price: 1,30
+		//Expected rsPercentSum = 3 month performance + 3 month perf. + 6 month perf. + 9 month perf + 12 month perf.
+		//= -22,29% -22,29% -8,72% -40,87% +4,62% = -88,67%
+		expectedRSPercentSum = (float) -89.55;
+		
+		actualRSPercentSum = indicatorCalculator.getRSPercentSum(this.dmlStock,sortedQuotations.get(0));
+		
+		assertEquals(expectedRSPercentSum, actualRSPercentSum);
 	}
 }
