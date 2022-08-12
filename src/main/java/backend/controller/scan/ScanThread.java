3c88b9c563c958e74dd1388291286c1b5e13be3b
MiRitter87 <michaelritter@gmx.net>
2022-08-12 09:11:50 +0200

    Initialize instrument relation of new quotations in ScanThread.

--- src/main/java/backend/controller/scan/ScanThread.java
+++ src/main/java/backend/controller/scan/ScanThread.java
@@ -177,8 +177,8 @@ public class ScanThread extends Thread {
 				databaseQuotation = instrument.getQuotationByDate(wsQuotation.getDate());
 				
 				if(databaseQuotation == null) {
+					wsQuotation.setInstrument(instrument);
 					newQuotations.add(wsQuotation);
-					instrument.addQuotation(wsQuotation);
 				}
 			}
 			
