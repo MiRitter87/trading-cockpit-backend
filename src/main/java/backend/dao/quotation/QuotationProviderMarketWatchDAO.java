824558554e6b035f9c009b01461a4df75e084eb7
MiRitter87 <michaelritter@gmx.net>
2022-10-24 09:22:17 +0200

    First version of Url determination for MarketWatch data query.

--- src/main/java/backend/dao/quotation/QuotationProviderMarketWatchDAO.java
+++ src/main/java/backend/dao/quotation/QuotationProviderMarketWatchDAO.java
@@ -1,5 +1,7 @@
 package backend.dao.quotation;
 
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 
 import backend.model.StockExchange;
@@ -11,6 +13,33 @@ import backend.model.instrument.Quotation;
  * @author Michael
  */
 public class QuotationProviderMarketWatchDAO implements QuotationProviderDAO {
+	/**
+	 * Placeholder for the symbol used in a query URL.
+	 */
+	private static final String PLACEHOLDER_SYMBOL = "{symbol}";	
+	
+	/**
+	 * Placeholder for the start date used in a query URL. The date format is mm/dd/yyyy.
+	 */
+	private static final String PLACEHOLDER_START_DATE = "{start_date}";
+	
+	/**
+	 * Placeholder for the end date used in a query URL. The date format is mm/dd/yyyy.
+	 */
+	private static final String PLACEHOLDER_END_DATE = "{end_date}";
+	
+	/**
+	 * Placeholder for the country code used in a query URL.
+	 */
+	private static final String PLACEHOLDER_COUNTRY_CODE = "{country_code}";
+	
+	/**
+	 * URL to CSV API of MarketWatch: Historical quotations.
+	 */
+	private static final String BASE_URL_QUOTATION_HISTORY = "https://www.marketwatch.com/investing/stock/" + PLACEHOLDER_SYMBOL + 
+			"/downloaddatapartial?startdate=" + PLACEHOLDER_START_DATE + "%2000:00:00&enddate=" + PLACEHOLDER_END_DATE + 
+			"%2023:59:59&daterange=d30&frequency=p1d&csvdownload=true&downloadpartial=false&newdates=false&countrycode=" + PLACEHOLDER_COUNTRY_CODE;
+	
 
 	@Override
 	public Quotation getCurrentQuotation(String symbol, StockExchange stockExchange) throws Exception {
@@ -25,4 +54,114 @@ public class QuotationProviderMarketWatchDAO implements QuotationProviderDAO {
 		return null;
 	}
 
+	
+	/**
+	 * Gets the query URL for the quotation history of the given symbol and stock exchange.
+	 * 
+	 * @param symbol The symbol to be queried.
+	 * @param stockExchange The stock exchange where the symbol is listed.
+	 * @param years The number of years to be queried.
+	 * @return The query URL.
+	 */
+	protected String getQueryUrlQuotationHistory(final String symbol, final StockExchange stockExchange, final Integer years) {
+		String queryUrl = new String(BASE_URL_QUOTATION_HISTORY);
+		
+		queryUrl = queryUrl.replace(PLACEHOLDER_SYMBOL, symbol);
+		queryUrl = queryUrl.replace(PLACEHOLDER_START_DATE, this.getStartDateForHistory());
+		queryUrl = queryUrl.replace(PLACEHOLDER_END_DATE, this.getEndDateForHistory());
+		queryUrl = queryUrl.replace(PLACEHOLDER_COUNTRY_CODE, this.getCountryCode(stockExchange));
+		
+		return queryUrl;
+	}
+	
+	
+	/**
+	 * Determines the start date for the quotation history.
+	 * 
+	 * @return The start date in the format mm/dd/yyyy.
+	 */
+	protected String getStartDateForHistory() {
+		StringBuilder stringBuilder = new StringBuilder();
+		Calendar calendar = Calendar.getInstance();
+		int day, month, year;
+		
+		calendar.setTime(new Date());
+		calendar.add(Calendar.YEAR, -1);
+		
+		day = calendar.get(Calendar.DAY_OF_MONTH);
+		month = calendar.get(Calendar.MONTH) + 1; //Add 1 because the first month of the year is returned as 0.
+		year = calendar.get(Calendar.YEAR);
+		
+		//Add a leading zero if day or month is returned as single-digit number.
+		if(month < 10)
+			stringBuilder.append("0");
+		
+		stringBuilder.append(month);
+		stringBuilder.append("/");
+		
+		if(day < 10)
+			stringBuilder.append("0");
+		
+		stringBuilder.append(day);
+		stringBuilder.append("/");
+		stringBuilder.append(year);
+		
+		return stringBuilder.toString();
+	}
+	
+	
+	/**
+	 * Determines the end date for the quotation history.
+	 * 
+	 * @return The start date in the format mm/dd/yyyy.
+	 */
+	protected String getEndDateForHistory() {
+		StringBuilder stringBuilder = new StringBuilder();
+		Calendar calendar = Calendar.getInstance();
+		int day, month, year;
+		
+		calendar.setTime(new Date());
+		
+		day = calendar.get(Calendar.DAY_OF_MONTH);
+		month = calendar.get(Calendar.MONTH) + 1; //Add 1 because the first month of the year is returned as 0.
+		year = calendar.get(Calendar.YEAR);
+		
+		//Add a leading zero if day or month is returned as single-digit number.
+		if(month < 10)
+			stringBuilder.append("0");
+		
+		stringBuilder.append(month);
+		stringBuilder.append("/");
+		
+		if(day < 10)
+			stringBuilder.append("0");
+		
+		stringBuilder.append(day);
+		stringBuilder.append("/");
+		stringBuilder.append(year);
+		
+		return stringBuilder.toString();
+	}
+	
+	
+	/**
+	 * Provides the country code for the given StockExchange.
+	 * 
+	 * @param stockExchange The StockExchange.
+	 * @return The country code.
+	 */
+	protected String getCountryCode(final StockExchange stockExchange) {
+		String countryCode = "";
+		
+		switch(stockExchange) {
+			case TSX:
+			case TSXV:
+				countryCode = "CA";
+				break;
+		default:
+			break;
+		}
+		
+		return countryCode;
+	}
 }
--- src/test/java/backend/dao/quotation/QuotationProviderMarketWatchDAOTest.java
+++ src/test/java/backend/dao/quotation/QuotationProviderMarketWatchDAOTest.java
@@ -1,7 +1,12 @@
 package backend.dao.quotation;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import backend.model.StockExchange;
 
 
 /**
@@ -31,5 +36,27 @@ public class QuotationProviderMarketWatchDAOTest {
 	 */
 	public static void tearDownClass() {
 		quotationProviderMarketWatchDAO = null;
+	}
+	
+	
+	@Test
+	/**
+	 * Tests the retrieval of the query URL for historical quotations of a stock listed at the TSX.
+	 */
+	public void testGetQueryUrlQuotationHistoryTSX() {
+		final String symbol = "DML";
+		final StockExchange stockExchange = StockExchange.TSX;
+		final Integer years = 1;
+		String expectedUrl = 	"https://www.marketwatch.com/investing/stock/DML/downloaddatapartial?"
+				+ "startdate={start_date}%2000:00:00&enddate={end_date}%2023:59:59&daterange=d30&frequency=p1d&csvdownload=true&"
+				+ "downloadpartial=false&newdates=false&countrycode=CA";
+		String actualUrl = "";
+		
+		//Replace start and end date with the current date.
+		expectedUrl = expectedUrl.replace("{start_date}", quotationProviderMarketWatchDAO.getStartDateForHistory());
+		expectedUrl = expectedUrl.replace("{end_date}", quotationProviderMarketWatchDAO.getEndDateForHistory());
+		
+		actualUrl = quotationProviderMarketWatchDAO.getQueryUrlQuotationHistory(symbol, stockExchange, years);
+		assertEquals(expectedUrl, actualUrl);
 	}
 }
