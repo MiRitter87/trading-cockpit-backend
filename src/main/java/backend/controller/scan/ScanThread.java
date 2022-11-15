1120e52e3778edc5c36694d674af8e879647836f
MiRitter87 <michaelritter@gmx.net>
2022-11-15 08:12:11 +0100

    Calculate and set volumeDifferential5Days in ScanThread.

--- src/main/java/backend/controller/scan/ScanThread.java
+++ src/main/java/backend/controller/scan/ScanThread.java
@@ -235,6 +235,7 @@ public class ScanThread extends Thread {
 			indicator.setDistanceTo52WeekHigh(this.indicatorCalculator.getDistanceTo52WeekHigh(mostRecentQuotation, sortedQuotations));
 			indicator.setDistanceTo52WeekLow(this.indicatorCalculator.getDistanceTo52WeekLow(mostRecentQuotation, sortedQuotations));
 			indicator.setBollingerBandWidth(this.indicatorCalculator.getBollingerBandWidth(10, 2, mostRecentQuotation, sortedQuotations));
+			indicator.setVolumeDifferential5Days(this.indicatorCalculator.getVolumeDifferential(30, 5, mostRecentQuotation, sortedQuotations));
 			indicator.setVolumeDifferential10Days(this.indicatorCalculator.getVolumeDifferential(30, 10, mostRecentQuotation, sortedQuotations));
 			indicator.setBaseLengthWeeks(this.indicatorCalculator.getBaseLengthWeeks(mostRecentQuotation, sortedQuotations));
 			indicator.setUpDownVolumeRatio(this.indicatorCalculator.getUpDownVolumeRatio(50, mostRecentQuotation, sortedQuotations));
