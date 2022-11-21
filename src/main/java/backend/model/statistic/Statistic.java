db8064f80cfbdcb8781893d32758eb429bb40e9b
MiRitter87 <michaelritter@gmx.net>
2022-11-21 08:33:38 +0100

    Add JPA annotations to Statistic model.

--- src/main/java/backend/model/statistic/Statistic.java
+++ src/main/java/backend/model/statistic/Statistic.java
@@ -3,6 +3,16 @@ package backend.model.statistic;
 import java.util.Date;
 import java.util.Objects;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
 import backend.model.instrument.InstrumentType;
 
 /**
@@ -10,35 +20,47 @@ import backend.model.instrument.InstrumentType;
  * 
  * @author Michael
  */
+@Table(name="STATISTIC")
+@Entity
+@SequenceGenerator(name = "statisticSequence", initialValue = 1, allocationSize = 1)
 public class Statistic {
 	/**
 	 * The ID.
 	 */
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "statisticSequence")
+	@Column(name="STATISTIC_ID")
 	private Integer id;
 	
 	/**
 	 * The date.
 	 */
+	@Column(name="DATE")
 	private Date date;
 	
 	/**
 	 * The InstrumentType.
 	 */
+	@Column(name="INSTRUMENT_TYPE", length = 10)
+	@Enumerated(EnumType.STRING)
 	private InstrumentType instrumentType;
 	
 	/**
 	 * The number of instruments which advanced since the last data point.
 	 */
+	@Column(name="NUMBER_ADVANCE")
 	private int numberAdvance;
 	
 	/**
 	 * The number of instruments which declined since the last data point.
 	 */
+	@Column(name="NUMBER_DECLINE")
 	private int numberDecline;
 	
 	/**
 	 * The sum of advancing and declining instruments since the last data point.
 	 */
+	@Column(name="ADVANCE_DECLINE_SUM")
 	private int advanceDeclineSum;
 	
 	
