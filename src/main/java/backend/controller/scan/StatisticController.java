eafbaaa224e12ce70ea12f21eb4e4b387e917c47
MiRitter87 <michaelritter@gmx.net>
2022-11-24 08:18:50 +0100

    Define methods in StatisticController needed to implement the process.

--- src/main/java/backend/controller/scan/StatisticController.java
+++ src/main/java/backend/controller/scan/StatisticController.java
@@ -1,5 +1,10 @@
 package backend.controller.scan;
 
+import java.util.List;
+
+import backend.model.instrument.Instrument;
+import backend.model.statistic.Statistic;
+
 /**
  * Controls the creation and update of statistical data.
  * 
@@ -40,5 +45,66 @@ public class StatisticController {
 		//-Check if statisticNew contains object with same date and type
 		//-if no, add Statistic to statisticDelete (this Statistic is old and not relevant anymore)
 		//perform DAOs method delete
+	}
+	
+	
+	/**
+	 * Calculates the statistics based on the given instruments.
+	 * 
+	 * @param instruments The instruments for which the statistics are to be calculated.
+	 * @return The statistics.
+	 */
+	public List<Statistic> calculateStatistics(final List<Instrument> instruments) {
+		return null;
+	}
+	
+	
+	/**
+	 * Persists statistics to the database.
+	 * 
+	 * @param newStatistics The new statistics that have been calculated during runtime based on the current database state of quotation data.
+	 * @throws Exception In case persisting failed.
+	 */
+	private void persistStatistics(final List <Statistic> newStatistics) throws Exception {
+		
+	}
+	
+	
+	/**
+	 * Compares the database state of the statistics and the newly calculated statistics.
+	 * New statistics are returned, that have yet to be persisted at database level.
+	 * 
+	 * @param newStatistics The new statistics that have been calculated during runtime based on the current database state of quotation data.
+	 * @param databaseStatistics The database state of the statistics before the new statistic calculation has been executed.
+	 * @return Statistics that have to be inserted into the database.
+	 */
+	private List<Statistic> getStatisticsForInsertion(final List <Statistic> newStatistics, final List <Statistic> databaseStatistics) {
+		return null;
+	}
+	
+	
+	/**
+	 * Compares the database state of the statistics and the newly calculated statistics.
+	 * Obsolete statistics are returned, that have to be deleted from the database.
+	 * 
+	 * @param newStatistics The new statistics that have been calculated during runtime based on the current database state of quotation data.
+	 * @param databaseStatistics The database state of the statistics before the new statistic calculation has been executed.
+	 * @return Statistics that have to be deleted from the database.
+	 */
+	private List<Statistic> getStatisticsForDeletion(final List <Statistic> newStatistics, final List <Statistic> databaseStatistics) {
+		return null;
+	}
+	
+	
+	/**
+	 * Compares the database state of the statistics and the newly calculated statistics.
+	 * Those statistics are returned that already existed at the database before but need to be updated with the newest data.
+	 * 
+	 * @param newStatistics The new statistics that have been calculated during runtime based on the current database state of quotation data.
+	 * @param databaseStatistics The database state of the statistics before the new statistic calculation has been executed.
+	 * @return Statistics that already exist but need to be updated.
+	 */
+	private List<Statistic> getStatisticsForUpdate(final List <Statistic> newStatistics, final List <Statistic> databaseStatistics) {
+		return null;
 	}
 }
