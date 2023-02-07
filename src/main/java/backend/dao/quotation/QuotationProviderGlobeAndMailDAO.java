1f937d12933bac848568ebd1edbe360ef80442d9
MiRitter87 <michaelritter@gmx.net>
2023-02-07 08:48:22 +0100

    Implement Quotation construction in QuotationProviderGlobeAndMailDAO.

--- src/main/java/backend/dao/quotation/QuotationProviderGlobeAndMailDAO.java
+++ src/main/java/backend/dao/quotation/QuotationProviderGlobeAndMailDAO.java
@@ -1,6 +1,10 @@
 package backend.dao.quotation;
 
+import java.math.BigDecimal;
 import java.util.List;
+
+import com.gargoylesoftware.htmlunit.html.DomElement;
+import com.gargoylesoftware.htmlunit.html.HtmlPage;
 
 import backend.model.StockExchange;
 import backend.model.instrument.Instrument;
@@ -23,8 +27,40 @@ public class QuotationProviderGlobeAndMailDAO extends AbstractQuotationProviderD
 	@Override
 	public List<Quotation> getQuotationHistory(String symbol, StockExchange stockExchange,
 			InstrumentType instrumentType, Integer years) throws Exception {
-		// TODO Auto-generated method stub
-		return null;
+
+		throw new Exception("Method is not supported.");
 	}
 
+	
+	/**
+	 * Gets the current Quotation from the HTML page.
+	 * 
+	 * @param htmlPage The HTML page containing the Quotation information.
+	 * @param instrument The Instrument for which Quotation data are extracted.
+	 * @return The current Quotation.
+	 * @throws Exception Failed to extract Quotation data from given HTML page.
+	 */
+	protected Quotation getQuotationFromHtmlPage(final HtmlPage htmlPage, final Instrument instrument) throws Exception {
+		Quotation quotation = new Quotation();
+		String currentPrice = "";
+		final List<DomElement> spans = htmlPage.getElementsByTagName("span");
+		
+		for (DomElement element : spans) {
+		    if (element.getAttribute("class").equals("barchart-overview-field-value")) {
+		    	DomElement firstChild = element.getFirstElementChild();
+		    	String nameAttribute = firstChild.getAttribute("name");
+
+		    	if(nameAttribute.equals("lastPrice"))
+		    		currentPrice = firstChild.getAttribute("value");
+		    }
+		}
+		
+		if("".equals(currentPrice))
+			throw new Exception("The price could not be determined.");
+		
+		quotation.setClose(new BigDecimal(currentPrice));
+		quotation.setCurrency(this.getCurrencyForStockExchange(instrument.getStockExchange()));
+		
+		return quotation;
+	}
 }
--- src/test/java/backend/dao/quotation/QuotationProviderGlobeAndMailDAOStub.java
+++ src/test/java/backend/dao/quotation/QuotationProviderGlobeAndMailDAOStub.java
@@ -1,18 +1,8 @@
 package backend.dao.quotation;
 
-import static org.junit.jupiter.api.Assertions.fail;
-
-import java.io.IOException;
-import java.math.BigDecimal;
-import java.net.MalformedURLException;
-import java.util.List;
-
-import com.gargoylesoftware.htmlunit.FailingHttpStatusCodeException;
 import com.gargoylesoftware.htmlunit.WebClient;
-import com.gargoylesoftware.htmlunit.html.DomElement;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
 
-import backend.model.Currency;
 import backend.model.StockExchange;
 import backend.model.instrument.Instrument;
 import backend.model.instrument.Quotation;
@@ -30,7 +20,7 @@ public class QuotationProviderGlobeAndMailDAOStub extends QuotationProviderGlobe
 		String htmlPath = "file:\\" + userPath;
 		WebClient webClient = new WebClient();
 		HtmlPage htmlPage;
-		Quotation quotation = new Quotation();
+		Quotation quotation;
 		
 		if(instrument.getSymbol().equals("PMET") && instrument.getStockExchange().equals(StockExchange.TSXV))
 			htmlPath = htmlPath + "\\src\\test\\resources\\GlobeAndMailTSXVQuotePMET.htm";
@@ -42,27 +32,7 @@ public class QuotationProviderGlobeAndMailDAOStub extends QuotationProviderGlobe
 			
 			htmlPage = webClient.getPage(htmlPath);
 			
-			final List<DomElement> spans = htmlPage.getElementsByTagName("span");
-			for (DomElement element : spans) {
-
-			    if (element.getAttribute("class").equals("barchart-overview-field-value")) {
-			    	DomElement firstChild = element.getFirstElementChild();
-			    	String nameAttribute = firstChild.getAttribute("name");
-
-			    	if(nameAttribute.equals("lastPrice"))
-			    		quotation.setClose(new BigDecimal(firstChild.getAttribute("value")));
-			    		quotation.setCurrency(Currency.CAD);
-			    }
-			}
-		} 
-		catch(FailingHttpStatusCodeException e) {
-			fail(e.getMessage());
-		} 
-		catch(MalformedURLException e) {
-			fail(e.getMessage());
-		} 
-		catch(IOException e) {
-			fail(e.getMessage());
+			quotation = this.getQuotationFromHtmlPage(htmlPage, instrument);	
 		}
 	    finally {
 	    	webClient.close();
