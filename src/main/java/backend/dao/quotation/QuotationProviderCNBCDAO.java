ffd1501db8bee0e29039b96a16045cdcc99d296f
MiRitter87 <michaelritter@gmx.net>
2023-02-23 09:12:36 +0100

    Implement URL construction for current quotation in CNBC DAO.

--- src/main/java/backend/dao/quotation/QuotationProviderCNBCDAO.java
+++ src/main/java/backend/dao/quotation/QuotationProviderCNBCDAO.java
@@ -13,7 +13,24 @@ import backend.model.instrument.Quotation;
  * @author Michael
  */
 public class QuotationProviderCNBCDAO implements QuotationProviderDAO {
-
+	/**
+	 * Placeholder for the symbol used in a query URL.
+	 */
+	private static final String PLACEHOLDER_SYMBOL = "{symbol}";
+	
+	/**
+	 * Placeholder for the country code used in a query URL.
+	 */
+	private static final String PLACEHOLDER_COUNTRY_CODE = "{country_code}";
+	
+	/**
+	 * URL to quote API of CNBC: Current quotation.
+	 */
+	private static final String BASE_URL_CURRENT_QUOTATION = "https://quote.cnbc.com/quote-html-webservice/restQuote/symbolType/symbol?"
+			+ "symbols=" + PLACEHOLDER_SYMBOL + PLACEHOLDER_COUNTRY_CODE + 
+			"&requestMethod=itv&noform=1&partnerId=2&fund=1&exthrs=1&output=json&events=1";
+	
+	
 	@Override
 	public Quotation getCurrentQuotation(Instrument instrument) throws Exception {
 		// TODO Auto-generated method stub
@@ -27,4 +44,51 @@ public class QuotationProviderCNBCDAO implements QuotationProviderDAO {
 		throw new Exception("Method is not supported.");
 	}
 
+	
+	/**
+	 * Gets the query URL for the current quotation of the given symbol and stock exchange.
+	 * 
+	 * @param symbol The symbol to be queried.
+	 * @param stockExchange The stock exchange where the symbol is listed.
+	 * @return The query URL.
+	 * @throws Exception In case the URL could not be determined.
+	 */
+	protected String getQueryUrlCurrentQuotation(final String symbol, final StockExchange stockExchange) throws Exception  {
+		String queryUrl = new String(BASE_URL_CURRENT_QUOTATION);
+		
+		queryUrl = queryUrl.replace(PLACEHOLDER_SYMBOL, symbol);
+		queryUrl = queryUrl.replace(PLACEHOLDER_COUNTRY_CODE, this.getCountryCodeParameter(stockExchange));
+		
+		return queryUrl;
+	}
+	
+	
+	/**
+	 * Provides the country code URL parameter for the given StockExchange.
+	 * 
+	 * @param stockExchange The StockExchange.
+	 * @return The country code URL parameter.
+	 * @Exception In case the StockExchange is not supported.
+	 */
+	protected String getCountryCodeParameter(final StockExchange stockExchange) throws Exception {
+		String countryCode = "";
+		
+		switch(stockExchange) {
+			case TSX:
+				countryCode = "-CA";
+				break;
+			case TSXV:
+				countryCode = "-V";
+				break;
+			case CSE:
+				throw new Exception("Stock Exchange 'CSE' is not supported by CNBC.");
+			case LSE:
+				countryCode = "-GB";
+				break;
+		default:
+			break;
+		}
+		
+		return countryCode;
+	}
 }
--- src/test/java/backend/dao/quotation/QuotationProviderCNBCDAOTest.java
+++ src/test/java/backend/dao/quotation/QuotationProviderCNBCDAOTest.java
@@ -1,7 +1,13 @@
 package backend.dao.quotation;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import backend.model.StockExchange;
 
 /**
  * Tests the CNBC Quotation DAO.
@@ -30,5 +36,107 @@ public class QuotationProviderCNBCDAOTest {
 	 */
 	public static void tearDownClass() {
 		quotationProviderCNBCDAO = null;
+	}
+	
+	
+	@Test
+	/**
+	 * Tests the retrieval of the query URL for the current quotation of a stock listed at the LSE.
+	 */
+	public void testGetQueryUrlCurrentQuotationLSE() {
+		final String symbol = "RIO";
+		final StockExchange stockExchange = StockExchange.LSE;
+		final String expectedURL = "https://quote.cnbc.com/quote-html-webservice/restQuote/symbolType/symbol?symbols=RIO-GB"
+				+ "&requestMethod=itv&noform=1&partnerId=2&fund=1&exthrs=1&output=json&events=1";
+		String actualURL = "";
+		
+		try {
+			actualURL = quotationProviderCNBCDAO.getQueryUrlCurrentQuotation(symbol, stockExchange);
+			assertEquals(expectedURL, actualURL);			
+		}
+		catch(Exception e) {
+			fail(e.getMessage());
+		}
+	}
+	
+	
+	@Test
+	/**
+	 * Tests the retrieval of the query URL for the current quotation of a stock listed at the NYSE.
+	 */
+	public void testGetQueryUrlCurrentQuotationNYSE() {
+		final String symbol = "AAPL";
+		final StockExchange stockExchange = StockExchange.NYSE;
+		final String expectedURL = "https://quote.cnbc.com/quote-html-webservice/restQuote/symbolType/symbol?symbols=AAPL"
+				+ "&requestMethod=itv&noform=1&partnerId=2&fund=1&exthrs=1&output=json&events=1";
+		String actualURL = "";
+		
+		try {
+			actualURL = quotationProviderCNBCDAO.getQueryUrlCurrentQuotation(symbol, stockExchange);
+			assertEquals(expectedURL, actualURL);			
+		}
+		catch(Exception e) {
+			fail(e.getMessage());
+		}
+	}
+	
+	
+	@Test
+	/**
+	 * Tests the retrieval of the query URL for the current quotation of a stock listed at the TSX.
+	 */
+	public void testGetQueryUrlCurrentQuotationTSX() {
+		final String symbol = "DML";
+		final StockExchange stockExchange = StockExchange.TSX;
+		final String expectedURL = "https://quote.cnbc.com/quote-html-webservice/restQuote/symbolType/symbol?symbols=DML-CA"
+				+ "&requestMethod=itv&noform=1&partnerId=2&fund=1&exthrs=1&output=json&events=1";
+		String actualURL = "";
+		
+		try {
+			actualURL = quotationProviderCNBCDAO.getQueryUrlCurrentQuotation(symbol, stockExchange);
+			assertEquals(expectedURL, actualURL);			
+		}
+		catch(Exception e) {
+			fail(e.getMessage());
+		}
+	}
+	
+	
+	@Test
+	/**
+	 * Tests the retrieval of the query URL for the current quotation of a stock listed at the TSX/V.
+	 */
+	public void testGetQueryUrlCurrentQuotationTSXV() {
+		final String symbol = "RCK";
+		final StockExchange stockExchange = StockExchange.TSXV;
+		final String expectedURL = "https://quote.cnbc.com/quote-html-webservice/restQuote/symbolType/symbol?symbols=RCK-V"
+				+ "&requestMethod=itv&noform=1&partnerId=2&fund=1&exthrs=1&output=json&events=1";
+		String actualURL = "";
+		
+		try {
+			actualURL = quotationProviderCNBCDAO.getQueryUrlCurrentQuotation(symbol, stockExchange);
+			assertEquals(expectedURL, actualURL);			
+		}
+		catch(Exception e) {
+			fail(e.getMessage());
+		}
+	}
+	
+	
+	@Test
+	/**
+	 * Tests the retrieval of the query URL for the current quotation of a stock listed at the CSE.
+	 */
+	public void testGetQueryUrlCurrentQuotationCSE() {
+		final String symbol = "AGN";
+		final StockExchange stockExchange = StockExchange.CSE;
+		
+		try {
+			quotationProviderCNBCDAO.getQueryUrlCurrentQuotation(symbol, stockExchange);
+			fail("URL determination should have failed because exchange 'CSE' is not supported.");	
+		}
+		catch(Exception expected) {
+			//All is well.
+		}
 	}
 }
