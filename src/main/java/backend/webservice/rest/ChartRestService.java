e39e94972247a5a92bfc4e8c3cefbe41e6ef97be
MiRitter87 <michaelritter@gmx.net>
2023-05-22 08:43:44 +0200

    Move charts from StatisticService to new ChartService.

---
+++ src/main/java/backend/webservice/rest/ChartRestService.java
@@ -0,0 +1,122 @@
+package backend.webservice.rest;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.Response;
+
+import backend.webservice.common.ChartService;
+
+/**
+ * WebService for Chart access using REST technology.
+ * 
+ * @author Michael
+ */
+@Path("/charts")
+public class ChartRestService {
+	/**
+	 * Provides a chart with the cumulative Advance Decline Number.
+	 * 
+	 * @param listId The ID of the list defining the instruments used chart creation (optional).
+	 * @return The chart.
+	 */
+    @GET
+    @Path("/cumulativeADNumber")
+    @Produces("image/png")
+	public Response getAdvanceDeclineNumberChart(@QueryParam("listId") final Integer listId) {
+    	ChartService chartService = new ChartService();
+    	return chartService.getAdvanceDeclineNumberChart(listId);
+	}
+    
+    
+    /**
+	 * Provides a chart with the percentage of instruments trading above their SMA(50).
+	 * 
+	 * @param listId The ID of the list defining the instruments used chart creation (optional).
+	 * @return The chart.
+	 */
+    @GET
+    @Path("/instrumentsAboveSma50")
+    @Produces("image/png")
+	public Response getInstrumentsAboveSma50Chart(@QueryParam("listId") final Integer listId) {
+    	ChartService chartService = new ChartService();
+    	return chartService.getInstrumentsAboveSma50Chart(listId);
+	}
+    
+    
+    /**
+	 * Provides a chart of an Instrument marked with Distribution Days.
+	 * 
+	 * @param instrumentId The ID of the Instrument used for chart creation.
+	 * @return The chart.
+	 */
+    @GET
+    @Path("/distributionDays/{id}")
+    @Produces("image/png")
+	public Response getDistributionDaysChart(@PathParam("instrumentId") final Integer instrumentId) {
+    	ChartService chartService = new ChartService();
+    	return chartService.getDistributionDaysChart(instrumentId);
+	}
+    
+    
+    /**
+	 * Provides a chart of an Instrument marked with Follow-Through Days.
+	 * 
+	 * @param instrumentId The ID of the Instrument used for chart creation.
+	 * @return The chart.
+	 */
+    @GET
+    @Path("/followThroughDays/{id}")
+    @Produces("image/png")
+	public Response getFollowThroughDaysChart(@PathParam("instrumentId") final Integer instrumentId) {
+    	ChartService chartService = new ChartService();
+    	return chartService.getFollowThroughDaysChart(instrumentId);
+	}
+    
+    
+    /**
+	 * Provides a chart of the Ritter Market Trend.
+	 * 
+	 * @param listId The ID of the list defining the instruments used chart creation (optional).
+	 * @return The chart.
+	 */
+    @GET
+    @Path("/ritterMarketTrend")
+    @Produces("image/png")
+	public Response getRitterMarketTrendChart(@QueryParam("listId") final Integer listId) {
+    	ChartService chartService = new ChartService();
+    	return chartService.getRitterMarketTrendChart(listId);
+	}
+    
+    
+    /**
+   	 * Provides a chart of the Ritter Pattern Indicator.
+   	 * 
+   	 * @param listId The ID of the list defining the instruments used chart creation (optional).
+   	 * @return The chart.
+   	 */
+       @GET
+       @Path("/ritterPatternIndicator")
+       @Produces("image/png")
+   	public Response getRitterPatternIndicatorChart(@QueryParam("listId") final Integer listId) {
+       	ChartService chartService = new ChartService();
+       	return chartService.getRitterPatternIndicatorChart(listId);
+   	}
+    
+    
+    /**
+   	 * Provides a chart of an Instrument marked with Pocket Pivots.
+   	 * 
+   	 * @param instrumentId The ID of the Instrument used for chart creation.
+   	 * @return The chart.
+   	 */
+       @GET
+       @Path("/pocketPivots/{id}")
+       @Produces("image/png")
+   	public Response getPocketPivotsChart(@PathParam("instrumentId") final Integer instrumentId) {
+       	ChartService chartService = new ChartService();
+       	return chartService.getPocketPivotsChart(instrumentId);
+   	}
+}
--- src/main/java/backend/webservice/rest/StatisticRestService.java
+++ src/main/java/backend/webservice/rest/StatisticRestService.java
@@ -5,11 +5,9 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
 
 import backend.model.instrument.InstrumentType;
 import backend.model.webservice.WebServiceResult;
-import backend.webservice.ChartType;
 import backend.webservice.common.StatisticService;
 
 /**
@@ -30,24 +28,5 @@ public class StatisticRestService {
 	public WebServiceResult getStatistics(@QueryParam("instrumentType") final InstrumentType instrumentType) {
 		StatisticService statisticService = new StatisticService();
 		return statisticService.getStatistics(instrumentType);
-	}
-	
-	
-	/**
-	 * Provides a chart of the requested statistical data.
-	 * 
-	 * @param chartType The type of the requested chart.
-	 * @param listId The ID of the list defining the instruments used for Statistic chart creation.
-	 * @param instrumentId The ID of the Instrument used for Statistic chart creation.
-	 * @return The chart.
-	 */
-    @GET
-    @Path("/chart")
-    @Produces("image/png")
-	public Response getStatisticsChart(@QueryParam("chartType") final ChartType chartType, @QueryParam("listId") final Integer listId,
-			@QueryParam("instrumentId") final Integer instrumentId) {
-    	
-    	StatisticService statisticService = new StatisticService();
-    	return statisticService.getStatisticsChart(chartType, listId, instrumentId);
 	}
 }
