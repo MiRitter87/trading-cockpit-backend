2adc6425da938054bf1ce7bcaee4c8f1d5db08c4
MiRitter87 <michaelritter@gmx.net>
2023-08-30 08:45:28 +0200

    Migrate queries of scan templates from Hibernate 5 to 6.

--- src/main/java/backend/dao/quotation/persistence/QuotationQueryProvider.java
+++ src/main/java/backend/dao/quotation/persistence/QuotationQueryProvider.java
@@ -38,7 +38,7 @@ public class QuotationQueryProvider {
     public Query getQueryForMinerviniTrendTemplate() {
         return this.entityManager
                 .createQuery("SELECT q FROM Quotation q JOIN FETCH q.instrument i JOIN q.indicator r WHERE "
-                        + "quotation_id IN :quotationIds " + "AND q.indicator IS NOT NULL " + "AND q.close > r.sma50 "
+                        + "q.id IN :quotationIds " + "AND q.indicator IS NOT NULL " + "AND q.close > r.sma50 "
                         + "AND r.sma50 > r.sma150 " + "AND r.sma150 > r.sma200 " + "AND r.distanceTo52WeekLow >= 30 "
                         + "AND r.distanceTo52WeekHigh >= -25 " + "AND r.rsNumber >= 70");
     }
@@ -51,7 +51,7 @@ public class QuotationQueryProvider {
     public Query getQueryForBreakoutCandidatesTemplate() {
         return this.entityManager
                 .createQuery("SELECT q FROM Quotation q JOIN FETCH q.instrument i JOIN q.indicator r WHERE "
-                        + "quotation_id IN :quotationIds " + "AND q.indicator IS NOT NULL "
+                        + "q.id IN :quotationIds " + "AND q.indicator IS NOT NULL "
                         + "AND r.volumeDifferential10Days < 0" + "AND r.bollingerBandWidth < 10 "
                         + "AND r.baseLengthWeeks >= 3" + "AND r.distanceTo52WeekHigh >= -10");
     }
@@ -64,7 +64,7 @@ public class QuotationQueryProvider {
     public Query getQueryForVolatilityContractionTemplate() {
         return this.entityManager
                 .createQuery("SELECT q FROM Quotation q JOIN FETCH q.instrument i JOIN q.indicator r WHERE "
-                        + "quotation_id IN :quotationIds " + "AND q.indicator IS NOT NULL "
+                        + "q.id IN :quotationIds " + "AND q.indicator IS NOT NULL "
                         + "AND r.volumeDifferential10Days < 0" + "AND r.bollingerBandWidth < 10");
     }
 
@@ -76,7 +76,7 @@ public class QuotationQueryProvider {
     public Query getQueryForUpOnVolumeTemplate() {
         return this.entityManager
                 .createQuery("SELECT q FROM Quotation q JOIN FETCH q.instrument i JOIN q.indicator r WHERE "
-                        + "quotation_id IN :quotationIds " + "AND q.indicator IS NOT NULL "
+                        + "q.id IN :quotationIds " + "AND q.indicator IS NOT NULL "
                         + "AND r.volumeDifferential5Days >= 25" + "AND r.performance5Days >= 10");
     }
 
@@ -88,7 +88,7 @@ public class QuotationQueryProvider {
     public Query getQueryForDownOnVolumeTemplate() {
         return this.entityManager
                 .createQuery("SELECT q FROM Quotation q JOIN FETCH q.instrument i JOIN q.indicator r WHERE "
-                        + "quotation_id IN :quotationIds " + "AND q.indicator IS NOT NULL "
+                        + "q.id IN :quotationIds " + "AND q.indicator IS NOT NULL "
                         + "AND r.volumeDifferential5Days >= 25" + "AND r.performance5Days <= -10");
     }
 
@@ -100,7 +100,7 @@ public class QuotationQueryProvider {
     public Query getQueryForNear52WeekHighTemplate() {
         return this.entityManager
                 .createQuery("SELECT q FROM Quotation q JOIN FETCH q.instrument i JOIN q.indicator r WHERE "
-                        + "quotation_id IN :quotationIds " + "AND q.indicator IS NOT NULL "
+                        + "q.id IN :quotationIds " + "AND q.indicator IS NOT NULL "
                         + "AND r.distanceTo52WeekHigh >= -5 ");
     }
 
@@ -112,7 +112,7 @@ public class QuotationQueryProvider {
     public Query getQueryForNear52WeekLowTemplate() {
         return this.entityManager
                 .createQuery("SELECT q FROM Quotation q JOIN FETCH q.instrument i JOIN q.indicator r WHERE "
-                        + "quotation_id IN :quotationIds " + "AND q.indicator IS NOT NULL "
+                        + "q.id IN :quotationIds " + "AND q.indicator IS NOT NULL "
                         + "AND r.distanceTo52WeekLow <= 5 ");
     }
 
@@ -124,7 +124,7 @@ public class QuotationQueryProvider {
     public Query getQueryForHighTightFlagTemplate() {
         return this.entityManager
                 .createQuery("SELECT q FROM Quotation q JOIN FETCH q.instrument i JOIN q.indicator r WHERE "
-                        + "quotation_id IN :quotationIds " + "AND q.indicator IS NOT NULL "
+                        + "q.id IN :quotationIds " + "AND q.indicator IS NOT NULL "
                         + "AND r.distanceTo52WeekHigh >= -25 ");
     }
 
@@ -136,7 +136,7 @@ public class QuotationQueryProvider {
     public Query getQueryForSwingTradingEnvironmentTemplate() {
         return this.entityManager
                 .createQuery("SELECT q FROM Quotation q JOIN FETCH q.instrument i JOIN q.indicator r WHERE "
-                        + "quotation_id IN :quotationIds " + "AND q.indicator IS NOT NULL " + "AND q.close > r.sma20 "
+                        + "q.id IN :quotationIds " + "AND q.indicator IS NOT NULL " + "AND q.close > r.sma20 "
                         + "AND r.sma10 > r.sma20 ");
     }
 
