7f1633164970c5d01b26a579faea462c56b2591c
MiRitter87 <michaelritter@gmx.net>
2023-08-31 09:35:09 +0200

    Migrate from servlet version 4 to 6.

--- src/main/java/backend/servlet/BootstrapContextListener.java
+++ src/main/java/backend/servlet/BootstrapContextListener.java
@@ -1,7 +1,7 @@
 package backend.servlet;
 
-import javax.servlet.ServletContextEvent;
-import javax.servlet.ServletContextListener;
+import jakarta.servlet.ServletContextEvent;
+import jakarta.servlet.ServletContextListener;
 
 import backend.controller.MainController;
 
