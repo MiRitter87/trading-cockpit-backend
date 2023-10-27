c27f835a4727386b4e4635559ec483b053b68e7c
MiRitter87 <michaelritter@gmx.net>
2023-10-27 08:18:38 +0200

    Move overlay creation of price volume chart to separate class.

---
+++ src/main/java/backend/controller/chart/priceVolume/ChartOverlayProvider.java
@@ -0,0 +1,192 @@
+package backend.controller.chart.priceVolume;
+
+import java.awt.Color;
+import java.util.List;
+import java.util.ResourceBundle;
+
+import org.jfree.chart.plot.DatasetRenderingOrder;
+import org.jfree.chart.plot.XYPlot;
+import org.jfree.chart.renderer.xy.XYItemRenderer;
+import org.jfree.chart.renderer.xy.XYLineAndShapeRenderer;
+import org.jfree.data.time.Day;
+import org.jfree.data.time.TimeSeries;
+import org.jfree.data.time.TimeSeriesCollection;
+
+import backend.model.instrument.Instrument;
+import backend.model.instrument.Quotation;
+
+/**
+ * Provides overlays for plots that are used in a Price Volume chart of an Instrument.
+ *
+ * @author Michael
+ */
+public class ChartOverlayProvider {
+    /**
+     * Access to localized application resources.
+     */
+    private ResourceBundle resources = ResourceBundle.getBundle("backend");
+
+    /**
+     * Adds the EMA(21) to the chart.
+     *
+     * @param instrument         The Instrument whose price and volume data are displayed.
+     * @param candleStickSubplot The Plot to which the EMA(21) is added.
+     */
+    public void addEma21(final Instrument instrument, final XYPlot candleStickSubplot) {
+        List<Quotation> quotationsSortedByDate = instrument.getQuotationsSortedByDate();
+        TimeSeriesCollection timeSeriesCollection = new TimeSeriesCollection();
+        TimeSeries ema21TimeSeries = new TimeSeries(this.resources.getString("chart.priceVolume.timeSeriesEma21Name"));
+        int index = candleStickSubplot.getDatasetCount();
+
+        for (Quotation tempQuotation : quotationsSortedByDate) {
+            if (tempQuotation.getIndicator().getEma21() == 0) {
+                continue;
+            }
+
+            ema21TimeSeries.add(new Day(tempQuotation.getDate()), tempQuotation.getIndicator().getEma21());
+        }
+
+        timeSeriesCollection.addSeries(ema21TimeSeries);
+
+        candleStickSubplot.setDataset(index, timeSeriesCollection);
+        candleStickSubplot.mapDatasetToRangeAxis(index, 0);
+
+        XYItemRenderer smaRenderer = new XYLineAndShapeRenderer(true, false);
+        smaRenderer.setSeriesPaint(0, Color.ORANGE);
+        candleStickSubplot.setRenderer(index, smaRenderer);
+        candleStickSubplot.setDatasetRenderingOrder(DatasetRenderingOrder.FORWARD);
+    }
+
+    /**
+     * Adds the SMA(50) to the chart.
+     *
+     * @param instrument         The Instrument whose price and volume data are displayed.
+     * @param candleStickSubplot The Plot to which the SMA(50) is added.
+     */
+    public void addSma50(final Instrument instrument, final XYPlot candleStickSubplot) {
+        List<Quotation> quotationsSortedByDate = instrument.getQuotationsSortedByDate();
+        TimeSeriesCollection timeSeriesCollection = new TimeSeriesCollection();
+        TimeSeries sma50TimeSeries = new TimeSeries(this.resources.getString("chart.priceVolume.timeSeriesSma50Name"));
+        int index = candleStickSubplot.getDatasetCount();
+
+        for (Quotation tempQuotation : quotationsSortedByDate) {
+            if (tempQuotation.getIndicator().getSma50() == 0) {
+                continue;
+            }
+
+            sma50TimeSeries.add(new Day(tempQuotation.getDate()), tempQuotation.getIndicator().getSma50());
+        }
+
+        timeSeriesCollection.addSeries(sma50TimeSeries);
+
+        candleStickSubplot.setDataset(index, timeSeriesCollection);
+        candleStickSubplot.mapDatasetToRangeAxis(index, 0);
+
+        XYItemRenderer smaRenderer = new XYLineAndShapeRenderer(true, false);
+        smaRenderer.setSeriesPaint(0, Color.BLUE);
+        candleStickSubplot.setRenderer(index, smaRenderer);
+        candleStickSubplot.setDatasetRenderingOrder(DatasetRenderingOrder.FORWARD);
+    }
+
+    /**
+     * Adds the SMA(150) to the chart.
+     *
+     * @param instrument         The Instrument whose price and volume data are displayed.
+     * @param candleStickSubplot The Plot to which the SMA(150) is added.
+     */
+    public void addSma150(final Instrument instrument, final XYPlot candleStickSubplot) {
+        List<Quotation> quotationsSortedByDate = instrument.getQuotationsSortedByDate();
+        TimeSeriesCollection timeSeriesCollection = new TimeSeriesCollection();
+        TimeSeries sma150TimeSeries = new TimeSeries(
+                this.resources.getString("chart.priceVolume.timeSeriesSma150Name"));
+        int index = candleStickSubplot.getDatasetCount();
+
+        for (Quotation tempQuotation : quotationsSortedByDate) {
+            if (tempQuotation.getIndicator().getSma150() == 0) {
+                continue;
+            }
+
+            sma150TimeSeries.add(new Day(tempQuotation.getDate()), tempQuotation.getIndicator().getSma150());
+        }
+
+        timeSeriesCollection.addSeries(sma150TimeSeries);
+
+        candleStickSubplot.setDataset(index, timeSeriesCollection);
+        candleStickSubplot.mapDatasetToRangeAxis(index, 0);
+
+        XYItemRenderer smaRenderer = new XYLineAndShapeRenderer(true, false);
+        smaRenderer.setSeriesPaint(0, Color.RED);
+        candleStickSubplot.setRenderer(index, smaRenderer);
+        candleStickSubplot.setDatasetRenderingOrder(DatasetRenderingOrder.FORWARD);
+    }
+
+    /**
+     * Adds the SMA(200) to the chart.
+     *
+     * @param instrument         The Instrument whose price and volume data are displayed.
+     * @param candleStickSubplot The Plot to which the SMA(200) is added.
+     */
+    public void addSma200(final Instrument instrument, final XYPlot candleStickSubplot) {
+        List<Quotation> quotationsSortedByDate = instrument.getQuotationsSortedByDate();
+        TimeSeriesCollection timeSeriesCollection = new TimeSeriesCollection();
+        TimeSeries sma200TimeSeries = new TimeSeries(
+                this.resources.getString("chart.priceVolume.timeSeriesSma200Name"));
+        int index = candleStickSubplot.getDatasetCount();
+
+        for (Quotation tempQuotation : quotationsSortedByDate) {
+            if (tempQuotation.getIndicator().getSma200() == 0) {
+                continue;
+            }
+
+            sma200TimeSeries.add(new Day(tempQuotation.getDate()), tempQuotation.getIndicator().getSma200());
+        }
+
+        timeSeriesCollection.addSeries(sma200TimeSeries);
+
+        candleStickSubplot.setDataset(index, timeSeriesCollection);
+        candleStickSubplot.mapDatasetToRangeAxis(index, 0);
+
+        XYItemRenderer smaRenderer = new XYLineAndShapeRenderer(true, false);
+        smaRenderer.setSeriesPaint(0, Color.GREEN);
+        candleStickSubplot.setRenderer(index, smaRenderer);
+        candleStickSubplot.setDatasetRenderingOrder(DatasetRenderingOrder.FORWARD);
+    }
+
+    /**
+     * Adds the SMA(30) of the volume to the chart.
+     *
+     * @param instrument      The Instrument whose price and volume data are displayed.
+     * @param withSma30Volume Show SMA(30) of volume.
+     * @param volumeSubplot   The Plot to which the SMA(30) of the volume is added.
+     */
+    public void addMovingAverageVolume(final Instrument instrument, final boolean withSma30Volume,
+            final XYPlot volumeSubplot) {
+        List<Quotation> quotationsSortedByDate = instrument.getQuotationsSortedByDate();
+        TimeSeriesCollection timeSeriesCollection = new TimeSeriesCollection();
+        TimeSeries sma30VolumeTimeSeries = new TimeSeries(
+                this.resources.getString("chart.priceVolume.timeSeriesSma30VolumeName"));
+        int index = volumeSubplot.getDatasetCount();
+
+        if (!withSma30Volume) {
+            return;
+        }
+
+        for (Quotation tempQuotation : quotationsSortedByDate) {
+            if (tempQuotation.getIndicator().getSma30Volume() == 0) {
+                continue;
+            }
+
+            sma30VolumeTimeSeries.add(new Day(tempQuotation.getDate()), tempQuotation.getIndicator().getSma30Volume());
+        }
+
+        timeSeriesCollection.addSeries(sma30VolumeTimeSeries);
+
+        volumeSubplot.setDataset(index, timeSeriesCollection);
+        volumeSubplot.mapDatasetToRangeAxis(index, 0);
+
+        XYItemRenderer smaRenderer = new XYLineAndShapeRenderer(true, false);
+        smaRenderer.setSeriesPaint(0, Color.BLACK);
+        volumeSubplot.setRenderer(index, smaRenderer);
+        volumeSubplot.setDatasetRenderingOrder(DatasetRenderingOrder.FORWARD);
+    }
+}
--- src/main/java/backend/controller/chart/priceVolume/PriceVolumeChartController.java
+++ src/main/java/backend/controller/chart/priceVolume/PriceVolumeChartController.java
@@ -18,12 +18,9 @@ import org.jfree.chart.axis.LogAxis;
 import org.jfree.chart.axis.NumberAxis;
 import org.jfree.chart.axis.ValueAxis;
 import org.jfree.chart.plot.CombinedDomainXYPlot;
-import org.jfree.chart.plot.DatasetRenderingOrder;
 import org.jfree.chart.plot.XYPlot;
 import org.jfree.chart.renderer.xy.CandlestickRenderer;
 import org.jfree.chart.renderer.xy.XYBarRenderer;
-import org.jfree.chart.renderer.xy.XYItemRenderer;
-import org.jfree.chart.renderer.xy.XYLineAndShapeRenderer;
 import org.jfree.data.time.Day;
 import org.jfree.data.time.TimeSeries;
 import org.jfree.data.time.TimeSeriesCollection;
@@ -65,6 +62,11 @@ public class PriceVolumeChartController extends ChartController {
     private ChartIndicatorProvider chartIndicatorProvider;
 
     /**
+     * Provider of chart overlays.
+     */
+    private ChartOverlayProvider chartOverlayProvider;
+
+    /**
      * Performance calculator.
      */
     private PerformanceCalculator performanceCalculator;
@@ -76,6 +78,7 @@ public class PriceVolumeChartController extends ChartController {
         this.chartObjectDAO = DAOManager.getInstance().getChartObjectDAO();
 
         this.chartIndicatorProvider = new ChartIndicatorProvider(this.getQuotationDAO());
+        this.chartOverlayProvider = new ChartOverlayProvider();
         this.performanceCalculator = new PerformanceCalculator();
     }
 
@@ -115,7 +118,7 @@ public class PriceVolumeChartController extends ChartController {
 
         if (withVolume) {
             volumeSubplot = this.getVolumePlot(instrument, dateAxis);
-            this.addMovingAverageVolume(instrument, withSma30Volume, volumeSubplot);
+            this.chartOverlayProvider.addMovingAverageVolume(instrument, withSma30Volume, volumeSubplot);
             this.clipVolumeAt2TimesAverage(volumeSubplot, instrument);
         }
 
@@ -408,186 +411,20 @@ public class PriceVolumeChartController extends ChartController {
         }
 
         if (overlays.contains(ChartOverlay.EMA_21.toString())) {
-            this.addEma21(instrument, candleStickSubplot);
+            this.chartOverlayProvider.addEma21(instrument, candleStickSubplot);
         }
 
         if (overlays.contains(ChartOverlay.SMA_50.toString())) {
-            this.addSma50(instrument, candleStickSubplot);
+            this.chartOverlayProvider.addSma50(instrument, candleStickSubplot);
         }
 
         if (overlays.contains(ChartOverlay.SMA_150.toString())) {
-            this.addSma150(instrument, candleStickSubplot);
+            this.chartOverlayProvider.addSma150(instrument, candleStickSubplot);
         }
 
         if (overlays.contains(ChartOverlay.SMA_200.toString())) {
-            this.addSma200(instrument, candleStickSubplot);
+            this.chartOverlayProvider.addSma200(instrument, candleStickSubplot);
         }
-    }
-
-    /**
-     * Adds the EMA(21) to the chart.
-     *
-     * @param instrument         The Instrument whose price and volume data are displayed.
-     * @param candleStickSubplot The Plot to which the EMA(21) is added.
-     */
-    private void addEma21(final Instrument instrument, final XYPlot candleStickSubplot) {
-        List<Quotation> quotationsSortedByDate = instrument.getQuotationsSortedByDate();
-        TimeSeriesCollection timeSeriesCollection = new TimeSeriesCollection();
-        TimeSeries ema21TimeSeries = new TimeSeries(
-                this.getResources().getString("chart.priceVolume.timeSeriesEma21Name"));
-        int index = candleStickSubplot.getDatasetCount();
-
-        for (Quotation tempQuotation : quotationsSortedByDate) {
-            if (tempQuotation.getIndicator().getEma21() == 0) {
-                continue;
-            }
-
-            ema21TimeSeries.add(new Day(tempQuotation.getDate()), tempQuotation.getIndicator().getEma21());
-        }
-
-        timeSeriesCollection.addSeries(ema21TimeSeries);
-
-        candleStickSubplot.setDataset(index, timeSeriesCollection);
-        candleStickSubplot.mapDatasetToRangeAxis(index, 0);
-
-        XYItemRenderer smaRenderer = new XYLineAndShapeRenderer(true, false);
-        smaRenderer.setSeriesPaint(0, Color.ORANGE);
-        candleStickSubplot.setRenderer(index, smaRenderer);
-        candleStickSubplot.setDatasetRenderingOrder(DatasetRenderingOrder.FORWARD);
-    }
-
-    /**
-     * Adds the SMA(50) to the chart.
-     *
-     * @param instrument         The Instrument whose price and volume data are displayed.
-     * @param candleStickSubplot The Plot to which the SMA(50) is added.
-     */
-    private void addSma50(final Instrument instrument, final XYPlot candleStickSubplot) {
-        List<Quotation> quotationsSortedByDate = instrument.getQuotationsSortedByDate();
-        TimeSeriesCollection timeSeriesCollection = new TimeSeriesCollection();
-        TimeSeries sma50TimeSeries = new TimeSeries(
-                this.getResources().getString("chart.priceVolume.timeSeriesSma50Name"));
-        int index = candleStickSubplot.getDatasetCount();
-
-        for (Quotation tempQuotation : quotationsSortedByDate) {
-            if (tempQuotation.getIndicator().getSma50() == 0) {
-                continue;
-            }
-
-            sma50TimeSeries.add(new Day(tempQuotation.getDate()), tempQuotation.getIndicator().getSma50());
-        }
-
-        timeSeriesCollection.addSeries(sma50TimeSeries);
-
-        candleStickSubplot.setDataset(index, timeSeriesCollection);
-        candleStickSubplot.mapDatasetToRangeAxis(index, 0);
-
-        XYItemRenderer smaRenderer = new XYLineAndShapeRenderer(true, false);
-        smaRenderer.setSeriesPaint(0, Color.BLUE);
-        candleStickSubplot.setRenderer(index, smaRenderer);
-        candleStickSubplot.setDatasetRenderingOrder(DatasetRenderingOrder.FORWARD);
-    }
-
-    /**
-     * Adds the SMA(150) to the chart.
-     *
-     * @param instrument         The Instrument whose price and volume data are displayed.
-     * @param candleStickSubplot The Plot to which the SMA(150) is added.
-     */
-    private void addSma150(final Instrument instrument, final XYPlot candleStickSubplot) {
-        List<Quotation> quotationsSortedByDate = instrument.getQuotationsSortedByDate();
-        TimeSeriesCollection timeSeriesCollection = new TimeSeriesCollection();
-        TimeSeries sma150TimeSeries = new TimeSeries(
-                this.getResources().getString("chart.priceVolume.timeSeriesSma150Name"));
-        int index = candleStickSubplot.getDatasetCount();
-
-        for (Quotation tempQuotation : quotationsSortedByDate) {
-            if (tempQuotation.getIndicator().getSma150() == 0) {
-                continue;
-            }
-
-            sma150TimeSeries.add(new Day(tempQuotation.getDate()), tempQuotation.getIndicator().getSma150());
-        }
-
-        timeSeriesCollection.addSeries(sma150TimeSeries);
-
-        candleStickSubplot.setDataset(index, timeSeriesCollection);
-        candleStickSubplot.mapDatasetToRangeAxis(index, 0);
-
-        XYItemRenderer smaRenderer = new XYLineAndShapeRenderer(true, false);
-        smaRenderer.setSeriesPaint(0, Color.RED);
-        candleStickSubplot.setRenderer(index, smaRenderer);
-        candleStickSubplot.setDatasetRenderingOrder(DatasetRenderingOrder.FORWARD);
-    }
-
-    /**
-     * Adds the SMA(200) to the chart.
-     *
-     * @param instrument         The Instrument whose price and volume data are displayed.
-     * @param candleStickSubplot The Plot to which the SMA(200) is added.
-     */
-    private void addSma200(final Instrument instrument, final XYPlot candleStickSubplot) {
-        List<Quotation> quotationsSortedByDate = instrument.getQuotationsSortedByDate();
-        TimeSeriesCollection timeSeriesCollection = new TimeSeriesCollection();
-        TimeSeries sma200TimeSeries = new TimeSeries(
-                this.getResources().getString("chart.priceVolume.timeSeriesSma200Name"));
-        int index = candleStickSubplot.getDatasetCount();
-
-        for (Quotation tempQuotation : quotationsSortedByDate) {
-            if (tempQuotation.getIndicator().getSma200() == 0) {
-                continue;
-            }
-
-            sma200TimeSeries.add(new Day(tempQuotation.getDate()), tempQuotation.getIndicator().getSma200());
-        }
-
-        timeSeriesCollection.addSeries(sma200TimeSeries);
-
-        candleStickSubplot.setDataset(index, timeSeriesCollection);
-        candleStickSubplot.mapDatasetToRangeAxis(index, 0);
-
-        XYItemRenderer smaRenderer = new XYLineAndShapeRenderer(true, false);
-        smaRenderer.setSeriesPaint(0, Color.GREEN);
-        candleStickSubplot.setRenderer(index, smaRenderer);
-        candleStickSubplot.setDatasetRenderingOrder(DatasetRenderingOrder.FORWARD);
-    }
-
-    /**
-     * Adds the SMA(30) of the volume to the chart.
-     *
-     * @param instrument      The Instrument whose price and volume data are displayed.
-     * @param withSma30Volume Show SMA(30) of volume.
-     * @param volumeSubplot   The Plot to which the SMA(30) of the volume is added.
-     */
-    private void addMovingAverageVolume(final Instrument instrument, final boolean withSma30Volume,
-            final XYPlot volumeSubplot) {
-        List<Quotation> quotationsSortedByDate = instrument.getQuotationsSortedByDate();
-        TimeSeriesCollection timeSeriesCollection = new TimeSeriesCollection();
-        TimeSeries sma30VolumeTimeSeries = new TimeSeries(
-                this.getResources().getString("chart.priceVolume.timeSeriesSma30VolumeName"));
-        int index = volumeSubplot.getDatasetCount();
-
-        if (!withSma30Volume) {
-            return;
-        }
-
-        for (Quotation tempQuotation : quotationsSortedByDate) {
-            if (tempQuotation.getIndicator().getSma30Volume() == 0) {
-                continue;
-            }
-
-            sma30VolumeTimeSeries.add(new Day(tempQuotation.getDate()), tempQuotation.getIndicator().getSma30Volume());
-        }
-
-        timeSeriesCollection.addSeries(sma30VolumeTimeSeries);
-
-        volumeSubplot.setDataset(index, timeSeriesCollection);
-        volumeSubplot.mapDatasetToRangeAxis(index, 0);
-
-        XYItemRenderer smaRenderer = new XYLineAndShapeRenderer(true, false);
-        smaRenderer.setSeriesPaint(0, Color.BLACK);
-        volumeSubplot.setRenderer(index, smaRenderer);
-        volumeSubplot.setDatasetRenderingOrder(DatasetRenderingOrder.FORWARD);
     }
 
     /**
