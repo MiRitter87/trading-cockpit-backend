25f4f04e12715515a1e7f96e1315b1a73143e30e
MiRitter87 <michaelritter@gmx.net>
2023-10-30 09:00:54 +0100

    New controller and initial test class for average instrument checks.

---
+++ src/main/java/backend/controller/instrumentCheck/InstrumentCheckAverageController.java
@@ -0,0 +1,11 @@
+package backend.controller.instrumentCheck;
+
+/**
+ * Controller that performs Instrument health checks in relation to moving averages. For example this can be a close
+ * below the SMA(50).
+ *
+ * @author Michael
+ */
+public class InstrumentCheckAverageController {
+
+}
