5095f42da6abe48cc4ae370233281763098fe0f8
MiRitter87 <michaelritter@gmx.net>
2023-10-31 08:12:53 +0100

    New controller and initial test class for climax instrument checks.

---
+++ src/main/java/backend/controller/instrumentCheck/InstrumentCheckClimaxController.java
@@ -0,0 +1,10 @@
+package backend.controller.instrumentCheck;
+
+/**
+ * Controller that performs Instrument health checks with regard to climax moves.
+ *
+ * @author Michael
+ */
+public class InstrumentCheckClimaxController {
+
+}
---
+++ src/test/java/backend/controller/instrumentCheck/InstrumentCheckClimaxControllerTest.java
@@ -0,0 +1,119 @@
+package backend.controller.instrumentCheck;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+import java.util.List;
+import java.util.ResourceBundle;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+
+import backend.controller.scan.IndicatorCalculator;
+import backend.dao.quotation.provider.QuotationProviderYahooDAO;
+import backend.dao.quotation.provider.QuotationProviderYahooDAOStub;
+import backend.model.StockExchange;
+import backend.model.instrument.Instrument;
+import backend.model.instrument.InstrumentType;
+import backend.model.instrument.Quotation;
+import backend.model.instrument.QuotationArray;
+
+/**
+ * Tests the InstrumentCheckClimaxController.
+ *
+ * @author Michael
+ */
+public class InstrumentCheckClimaxControllerTest {
+    /**
+     * Access to localized application resources.
+     */
+    private ResourceBundle resources = ResourceBundle.getBundle("backend");
+
+    /**
+     * DAO to access quotation data from Yahoo.
+     */
+    private static QuotationProviderYahooDAO quotationProviderYahooDAO;
+
+    /**
+     * A list of quotations of the DML stock.
+     */
+    private QuotationArray dmlQuotations;
+
+    /**
+     * The controller for climax-related Instrument health checks.
+     */
+    private InstrumentCheckClimaxController instrumentCheckClimaxController;
+
+    @BeforeAll
+    /**
+     * Tasks to be performed once at startup of test class.
+     */
+    public static void setUpClass() {
+        quotationProviderYahooDAO = new QuotationProviderYahooDAOStub();
+    }
+
+    @AfterAll
+    /**
+     * Tasks to be performed once at end of test class.
+     */
+    public static void tearDownClass() {
+        quotationProviderYahooDAO = null;
+    }
+
+    @BeforeEach
+    /**
+     * Tasks to be performed before each test is run.
+     */
+    private void setUp() {
+        this.instrumentCheckClimaxController = new InstrumentCheckClimaxController();
+
+        this.initializeDMLQuotations();
+        this.initializeDMLIndicators();
+    }
+
+    @AfterEach
+    /**
+     * Tasks to be performed after each test has been run.
+     */
+    private void tearDown() {
+        this.instrumentCheckClimaxController = null;
+        this.dmlQuotations = null;
+    }
+
+    /**
+     * Initializes quotations of the DML stock.
+     */
+    private void initializeDMLQuotations() {
+        try {
+            this.dmlQuotations = new QuotationArray();
+            this.dmlQuotations.setQuotations(
+                    quotationProviderYahooDAO.getQuotationHistory("DML", StockExchange.TSX, InstrumentType.STOCK, 1));
+        } catch (Exception e) {
+            fail(e.getMessage());
+        }
+    }
+
+    /**
+     * Initializes the indicators of the DML stock.
+     */
+    private void initializeDMLIndicators() {
+        IndicatorCalculator indicatorCalculator = new IndicatorCalculator();
+        List<Quotation> sortedQuotations;
+        Instrument instrument = new Instrument();
+        Quotation quotation;
+
+        instrument.setQuotations(this.dmlQuotations.getQuotations());
+        sortedQuotations = instrument.getQuotationsSortedByDate();
+
+        for (int i = 0; i < sortedQuotations.size(); i++) {
+            quotation = sortedQuotations.get(i);
+
+            // Calculate all Indicators only for most recent Quotation like in the ScanThread.
+            if (i == 0)
+                quotation = indicatorCalculator.calculateIndicators(instrument, quotation, true);
+            else
+                quotation = indicatorCalculator.calculateIndicators(instrument, quotation, false);
+        }
+    }
+}
