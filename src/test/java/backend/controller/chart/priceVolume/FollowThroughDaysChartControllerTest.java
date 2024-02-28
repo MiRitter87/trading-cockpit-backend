56cdeac9806ca26aacf9ac9a970cadf4abd9761f
MiRitter87 <michaelritter@gmx.net>
2024-02-28 09:08:42 +0100

    Initial version of FollowThroughDaysChartControllerTest created.

---
+++ src/test/java/backend/controller/chart/priceVolume/FollowThroughDaysChartControllerTest.java
@@ -0,0 +1,103 @@
+package backend.controller.chart.priceVolume;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+
+import backend.dao.quotation.provider.QuotationProviderDAO;
+import backend.dao.quotation.provider.QuotationProviderYahooDAOStub;
+import backend.model.StockExchange;
+import backend.model.instrument.Instrument;
+import backend.model.instrument.InstrumentType;
+import backend.model.instrument.Quotation;
+
+/**
+ * Tests the FollowThroughDaysChartController.
+ *
+ * @author Michael
+ */
+public class FollowThroughDaysChartControllerTest {
+    /**
+     * The FollowThroughDaysChartController under test.
+     */
+    private FollowThroughDaysChartController followThroughDaysChartController;
+
+    /**
+     * A trading instrument with quotations.
+     */
+    private Instrument dmlStock;
+
+    /**
+     * DAO to access quotation data from Yahoo.
+     */
+    private static QuotationProviderDAO quotationProviderYahooDAO;
+
+    @BeforeAll
+    /**
+     * Tasks to be performed once at startup of test class.
+     */
+    public static void setUpClass() {
+        try {
+            quotationProviderYahooDAO = new QuotationProviderYahooDAOStub();
+        } catch (Exception e) {
+            fail(e.getMessage());
+        }
+    }
+
+    @AfterAll
+    /**
+     * Tasks to be performed once at the end of the test class.
+     */
+    public static void tearDownClass() {
+        quotationProviderYahooDAO = null;
+    }
+
+    @BeforeEach
+    /**
+     * Tasks to be performed before each test is run.
+     */
+    private void setUp() {
+        try {
+            this.followThroughDaysChartController = new FollowThroughDaysChartController();
+            this.initializeDmlInstrument();
+        } catch (Exception e) {
+            fail(e.getMessage());
+        }
+    }
+
+    @AfterEach
+    /**
+     * Tasks to be performed after each test has been run.
+     */
+    private void tearDown() {
+        this.followThroughDaysChartController = null;
+        this.dmlStock = null;
+    }
+
+    /**
+     * Initializes the DML Instrument.
+     */
+    private void initializeDmlInstrument() {
+        List<Quotation> quotations = new ArrayList<>();
+
+        this.dmlStock = new Instrument();
+        this.dmlStock.setSymbol("DML");
+        this.dmlStock.setStockExchange(StockExchange.TSX);
+        this.dmlStock.setType(InstrumentType.STOCK);
+        this.dmlStock.setName("Denison Mines");
+
+        try {
+            quotations.addAll(
+                    quotationProviderYahooDAO.getQuotationHistory("DML", StockExchange.TSX, InstrumentType.STOCK, 1));
+            this.dmlStock.setQuotations(quotations);
+        } catch (Exception e) {
+            fail(e.getMessage());
+        }
+    }
+}
