090fa2af35b024018fda66439d040f4e0154333a
MiRitter87 <michaelritter@gmx.net>
2024-03-05 08:41:06 +0100

    Initial version of AverageTrueRangeCalculatorTest created.

---
+++ src/main/java/backend/controller/scan/AverageTrueRangeCalculator.java
@@ -0,0 +1,10 @@
+package backend.controller.scan;
+
+/**
+ * Performs the calculation of the Average True Range (Percent) indicator.
+ *
+ * @author Michael
+ */
+public class AverageTrueRangeCalculator {
+
+}
---
+++ src/test/java/backend/controller/scan/AverageTrueRangeCalculatorTest.java
@@ -0,0 +1,104 @@
+package backend.controller.scan;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+
+import backend.dao.quotation.provider.QuotationProviderDAO;
+import backend.dao.quotation.provider.QuotationProviderYahooDAOStub;
+import backend.model.StockExchange;
+import backend.model.instrument.Instrument;
+import backend.model.instrument.InstrumentType;
+import backend.model.instrument.Quotation;
+
+/**
+ * Tests the AverageTrueRangeCalculator.
+ *
+ * @author Michael
+ */
+public class AverageTrueRangeCalculatorTest {
+    /**
+     * The AverageTrueRangeCalculator under test.
+     */
+    private AverageTrueRangeCalculator averageTrueRangeCalculator;
+
+    /**
+     * A trading instrument whose indicators are calculated.
+     */
+    private Instrument dmlStock;
+
+    /**
+     * DAO to access quotation data from Yahoo.
+     */
+    private static QuotationProviderDAO quotationProviderYahooDAO;
+
+    @BeforeAll
+    /**
+     * Tasks to be performed once at startup of test class.
+     */
+    public static void setUpClass() {
+        try {
+            quotationProviderYahooDAO = new QuotationProviderYahooDAOStub();
+        } catch (Exception e) {
+            fail(e.getMessage());
+        }
+    }
+
+    @AfterAll
+    /**
+     * Tasks to be performed once at the end of the test class.
+     */
+    public static void tearDownClass() {
+        quotationProviderYahooDAO = null;
+    }
+
+    @BeforeEach
+    /**
+     * Tasks to be performed before each test is run.
+     */
+    private void setUp() {
+        try {
+            this.averageTrueRangeCalculator = new AverageTrueRangeCalculator();
+            this.initializeDmlInstrument();
+        } catch (Exception e) {
+            fail(e.getMessage());
+        }
+    }
+
+    @AfterEach
+    /**
+     * Tasks to be performed after each test has been run.
+     */
+    private void tearDown() {
+        this.averageTrueRangeCalculator = null;
+
+        this.dmlStock = null;
+    }
+
+    /**
+     * Initializes the DML Instrument.
+     */
+    private void initializeDmlInstrument() {
+        List<Quotation> quotations = new ArrayList<>();
+
+        this.dmlStock = new Instrument();
+        this.dmlStock.setSymbol("DML");
+        this.dmlStock.setStockExchange(StockExchange.TSX);
+        this.dmlStock.setType(InstrumentType.STOCK);
+        this.dmlStock.setName("Denison Mines");
+
+        try {
+            quotations.addAll(
+                    quotationProviderYahooDAO.getQuotationHistory("DML", StockExchange.TSX, InstrumentType.STOCK, 1));
+            this.dmlStock.setQuotations(quotations);
+        } catch (Exception e) {
+            fail(e.getMessage());
+        }
+    }
+}
