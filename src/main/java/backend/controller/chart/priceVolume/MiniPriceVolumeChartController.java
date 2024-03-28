f6241cf76ea4dd381c83ca05b45b9583881b5009
MiRitter87 <michaelritter@gmx.net>
2024-03-28 08:31:48 +0100

    Implement creation of mini price/volume chart.

--- src/main/java/backend/controller/chart/priceVolume/MiniPriceVolumeChartController.java
+++ src/main/java/backend/controller/chart/priceVolume/MiniPriceVolumeChartController.java
@@ -1,8 +1,12 @@
 package backend.controller.chart.priceVolume;
 
 import org.jfree.chart.JFreeChart;
+import org.jfree.chart.axis.DateAxis;
+import org.jfree.chart.plot.CombinedDomainXYPlot;
+import org.jfree.chart.plot.XYPlot;
 
 import backend.controller.NoQuotationsExistException;
+import backend.model.instrument.Instrument;
 
 /**
  * Controller for the creation of a miniature chart displaying an Instrument with price and volume. The miniature
@@ -13,6 +17,11 @@ import backend.controller.NoQuotationsExistException;
  */
 public class MiniPriceVolumeChartController extends PriceVolumeChartController {
     /**
+     * The number of trading days to be displayed (3 month period).
+     */
+    public static final Integer TRADING_DAYS_3_MONTHS = 63;
+
+    /**
      * Gets a miniature price/volume chart of an Instrument.
      *
      * @param instrumentId The ID of the Instrument used for chart creation.
@@ -20,7 +29,32 @@ public class MiniPriceVolumeChartController extends PriceVolumeChartController {
      * @throws NoQuotationsExistException No quotations exist for the Quotation with the given ID.
      * @throws Exception                  Chart generation failed.
      */
-    public JFreeChart getPocketPivotsChart(final Integer instrumentId) throws NoQuotationsExistException, Exception {
-        return null;
+    public JFreeChart getMiniPriceVolumeChart(final Integer instrumentId) throws NoQuotationsExistException, Exception {
+        Instrument instrument = this.getInstrumentWithQuotations(instrumentId, TRADING_DAYS_3_MONTHS);
+        JFreeChart chart;
+        DateAxis dateAxis = this.getDateAxis(instrument); // The shared time axis of all subplots.
+        final int candleStickPlotWeight = 4;
+        CombinedDomainXYPlot combinedPlot = new CombinedDomainXYPlot();
+
+        XYPlot indicatorSubplot = this.getChartIndicatorProvider().getBollingerBandWidthPlot(instrument, dateAxis);
+        XYPlot candleStickSubplot = this.getCandlestickPlot(instrument, dateAxis);
+        XYPlot volumeSubplot = this.getVolumePlot(instrument, dateAxis);
+
+        this.getChartOverlayProvider().addEma21(instrument, candleStickSubplot);
+        this.getChartOverlayProvider().addSma50(instrument, candleStickSubplot);
+
+        this.getChartOverlayProvider().addMovingAverageVolume(instrument, true, volumeSubplot);
+        this.clipVolumeAt2TimesAverage(volumeSubplot, instrument);
+
+        // Build combined plot based on subplots.
+        combinedPlot.setDomainAxis(dateAxis);
+        combinedPlot.add(indicatorSubplot, 1); // Indicator Plot takes 1 vertical size unit.
+        combinedPlot.add(candleStickSubplot, candleStickPlotWeight); // Price Plot takes 4 vertical size units.
+        combinedPlot.add(volumeSubplot, 1); // Volume Plot takes 1 vertical size unit.
+
+        // Build chart based on combined Plot.
+        chart = new JFreeChart(instrument.getName(), JFreeChart.DEFAULT_TITLE_FONT, combinedPlot, true);
+
+        return chart;
     }
 }
