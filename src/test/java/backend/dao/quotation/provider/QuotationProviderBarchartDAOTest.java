ef4b86e578f053099453ee1182e606808e4aa0f8
MiRitter87 <michaelritter@gmx.net>
2024-04-24 09:38:58 +0200

    Set additional header fields in explorative Barchart test.

--- src/test/java/backend/dao/quotation/provider/QuotationProviderBarchartDAOTest.java
+++ src/test/java/backend/dao/quotation/provider/QuotationProviderBarchartDAOTest.java
@@ -17,7 +17,7 @@ import com.gargoylesoftware.htmlunit.util.Cookie;
 
 /**
  * Explorative tests trying to query EOD data from barchart service that is being protected via session cookies.
- * 
+ *
  * @author Michael
  */
 public class QuotationProviderBarchartDAOTest {
@@ -33,19 +33,19 @@ public class QuotationProviderBarchartDAOTest {
 	    Set<Cookie> cookies;
 	    Iterator<Cookie> cookieIterator;
 	    Cookie cookie;
-	    
+
 	    webClient.getOptions().setUseInsecureSSL(true);
 	    webClient.getOptions().setCssEnabled(false);
 	    webClient.getOptions().setJavaScriptEnabled(false);
 	    cookieManager.setCookiesEnabled(true);
-	    
+
 		try {
 			try {
 				htmlPage = webClient.getPage(url);
-				
+
 				//Cookies can be retrieved via the webClient
 				cookies = cookieManager.getCookies();
-				
+
 				cookieIterator = cookies.iterator();
 				while(cookieIterator.hasNext()) {
 					cookie = cookieIterator.next();
@@ -64,8 +64,8 @@ public class QuotationProviderBarchartDAOTest {
 	    	webClient.close();
 	    }
 	}
-	
-	
+
+
 	//@Test
 	/**
 	 * Requests EOD data using the cookies from the initial request.
@@ -76,37 +76,42 @@ public class QuotationProviderBarchartDAOTest {
 	    HtmlPage htmlPage;
 	    CookieManager cookieManager = webClient.getCookieManager();
 	    Set<Cookie> cookies;
-	    
+
 	    webClient.getOptions().setUseInsecureSSL(true);
 	    webClient.getOptions().setCssEnabled(false);
 	    webClient.getOptions().setJavaScriptEnabled(false);
 	    cookieManager.setCookiesEnabled(true);
-	    
+
 		try {
 			try {
 				htmlPage = webClient.getPage(url);
-				
+
 				//Cookies can be retrieved via the webClient
 				cookies = cookieManager.getCookies();
-				
+
 				//Query historical EOD data.
 				URL eodRequestUrl = new URL("https://www.barchart.com/proxies/timeseries/queryeod.ashx?symbol=AMZN&data=daily&maxrecords=640"
 			    		+ "&volume=contract&order=asc&dividends=false&backadjust=false&daystoexpiration=1&contractroll=expiration");
-				
+
 				//Set header parameter x-xsrf-token from previously retrieved cookie
 				WebRequest eodDataRequest = new WebRequest(eodRequestUrl);
-				
+
+				eodDataRequest.setAdditionalHeader("Accept", "*/*");
+				eodDataRequest.setAdditionalHeader("Accept-Language", "de,en-US;q=0.7,en;q=0.3");
+				eodDataRequest.setAdditionalHeader("Accept-Encoding", "gzip, deflate, br");
 				eodDataRequest.setAdditionalHeader("Connection", "keep-alive");
 				eodDataRequest.setAdditionalHeader("Cookie", this.getCookiesAsString(cookies));
+				eodDataRequest.setAdditionalHeader("DNT", "1");
 				eodDataRequest.setAdditionalHeader("Host", "www.barchart.com");
 				eodDataRequest.setAdditionalHeader("Referer", "https://www.barchart.com/stocks/quotes/AMZN/overview");
 				eodDataRequest.setAdditionalHeader("Sec-Fetch-Dest", "empty");
 				eodDataRequest.setAdditionalHeader("Sec-Fetch-Mode", "cors");
 				eodDataRequest.setAdditionalHeader("Sec-Fetch-Site", "same-origin");
+				eodDataRequest.setAdditionalHeader("Sec-GPC", "1");
 				eodDataRequest.setAdditionalHeader("TE", "trailers");
-				eodDataRequest.setAdditionalHeader("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/115.0");
+				eodDataRequest.setAdditionalHeader("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:123.0) Gecko/20100101 Firefox/123.0");
 				eodDataRequest.setAdditionalHeader("x-xsrf-token", this.getXsrfToken(cookies));
-				
+
 				htmlPage = webClient.getPage(eodRequestUrl);
 			} catch (FailingHttpStatusCodeException e) {
 				fail(e.getMessage());
@@ -120,11 +125,11 @@ public class QuotationProviderBarchartDAOTest {
 	    	webClient.close();
 	    }
 	}
-	
-	
+
+
 	/**
 	 * Gets the XSRF-Token from the Set of cookies.
-	 * 
+	 *
 	 * @param cookies A Set of cookies.
 	 * @return The XSRF-Token.
 	 */
@@ -132,22 +137,22 @@ public class QuotationProviderBarchartDAOTest {
 		Iterator<Cookie> cookieIterator;
 		Cookie cookie;
 		String xsrfToken = "";
-		
+
 		cookieIterator = cookies.iterator();
 		while(cookieIterator.hasNext()) {
 			cookie = cookieIterator.next();
-			
+
 			if(cookie.getName().equals("XSRF-TOKEN"))
 				xsrfToken = cookie.getValue();
 		}
-		
+
 		return xsrfToken;
 	}
-	
-	
+
+
 	/**
 	 * Gets the cookies as String. The String is formatted for sending via a request header.
-	 * 
+	 *
 	 * @param cookies A Set of cookies.
 	 * @return The cookies as String.
 	 */
@@ -155,20 +160,20 @@ public class QuotationProviderBarchartDAOTest {
 		Iterator<Cookie> cookieIterator;
 		Cookie cookie;
 		String cookieString = "";
-		
+
 		cookieIterator = cookies.iterator();
 		while(cookieIterator.hasNext()) {
 			cookie = cookieIterator.next();
-			
+
 			cookieString += cookie.getName();
 			cookieString += "=";
 			cookieString += cookie.getValue();
 			cookieString += "; ";
-			
+
 			if(!cookieIterator.hasNext())
 				cookieString += "bcFreeUserPageView=0; webinar152WebinarClosed=true";
 		}
-		
+
 		return cookieString;
 	}
 }
