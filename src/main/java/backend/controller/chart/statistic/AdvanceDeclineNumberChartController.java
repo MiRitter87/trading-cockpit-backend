1d6cbccaf3fd0c7d24521787d3a38000eeb6f818
MiRitter87 <michaelritter@gmx.net>
2024-04-29 08:46:21 +0200

    Prepare methods for SMA(50) calculation of cumulative A/D number.

--- src/main/java/backend/controller/chart/statistic/AdvanceDeclineNumberChartController.java
+++ src/main/java/backend/controller/chart/statistic/AdvanceDeclineNumberChartController.java
@@ -1,7 +1,9 @@
 package backend.controller.chart.statistic;
 
+import java.math.BigDecimal;
 import java.util.List;
 import java.util.ListIterator;
+import java.util.ResourceBundle;
 import java.util.TimeZone;
 
 import org.jfree.chart.ChartFactory;
@@ -14,6 +16,8 @@ import org.jfree.data.time.TimeSeriesCollection;
 import org.jfree.data.xy.XYDataset;
 
 import backend.model.instrument.InstrumentType;
+import backend.model.instrument.Quotation;
+import backend.model.instrument.QuotationArray;
 import backend.model.statistic.Statistic;
 
 /**
@@ -22,6 +26,11 @@ import backend.model.statistic.Statistic;
  * @author Michael
  */
 public class AdvanceDeclineNumberChartController extends StatisticChartController {
+    /**
+     * Access to localized application resources.
+     */
+    private ResourceBundle resources = ResourceBundle.getBundle("backend");
+
     /**
      * Gets a chart of the cumulative Advance/Decline Number.
      *
@@ -38,6 +47,7 @@ public class AdvanceDeclineNumberChartController extends StatisticChartControlle
         JFreeChart chart = ChartFactory.createTimeSeriesChart(this.getResources().getString("chart.adNumber.titleName"),
                 null, null, dataset, true, true, false);
 
+        this.addSma50(chart, statistics);
         this.applyBackgroundTheme(chart);
         chart.getXYPlot().setRangeAxisLocation(AxisLocation.TOP_OR_RIGHT);
 
@@ -71,5 +81,58 @@ public class AdvanceDeclineNumberChartController extends StatisticChartControlle
         timeSeriesCollection.setXPosition(TimePeriodAnchor.MIDDLE);
 
         return timeSeriesCollection;
+    }
+
+    /**
+     * Adds the SMA(50) to the chart.
+     *
+     * @param chart      The cumulative Advance/Decline number chart.
+     * @param statistics Statistics used for cumulative A/D number calculation.
+     */
+    private void addSma50(final JFreeChart chart, final List<Statistic> statistics) {
+        TimeSeriesCollection sma50TimeSeriesCollection;
+
+        sma50TimeSeriesCollection = this.getSma50TimeSeriesCollection(statistics);
+    }
+
+    /**
+     * Provides a TimeSeriesCollection with the values of the SMA(50).
+     *
+     * @param statistics Statistics used for cumulative A/D number calculation.
+     * @return The TimeSeriesCollection with the values of the SMA(50).
+     */
+    private TimeSeriesCollection getSma50TimeSeriesCollection(final List<Statistic> statistics) {
+        TimeSeriesCollection timeSeriesCollection = new TimeSeriesCollection();
+        TimeSeries sma50TimeSeries = new TimeSeries(this.resources.getString("chart.adNumber.timeSeriesSma50Name"));
+        QuotationArray quotationArray = this.getAdNumbersAsQuotationArray(statistics);
+
+        return null;
+    }
+
+    /**
+     * Provides a QuotationArray where the cumulative Advance/Decline number is stored in the closing price of each
+     * Quotation. This way the SMA(50) can be calculated easily using existing methods.
+     *
+     * @param statistics Statistics used for cumulative A/D number calculation.
+     * @return A QuotationArray.
+     */
+    private QuotationArray getAdNumbersAsQuotationArray(final List<Statistic> statistics) {
+        Quotation quotation;
+        QuotationArray quotationArray = new QuotationArray();
+        ListIterator<Statistic> iterator;
+        int cumulativeADNumber = 0;
+        Statistic statistic;
+
+        iterator = statistics.listIterator(statistics.size());
+        while (iterator.hasPrevious()) {
+            statistic = iterator.previous();
+            cumulativeADNumber += statistic.getAdvanceDeclineNumber();
+            quotation = new Quotation();
+            quotation.setClose(new BigDecimal(cumulativeADNumber));
+            quotation.setDate(statistic.getDate());
+            quotationArray.getQuotations().add(quotation);
+        }
+
+        return quotationArray;
     }
 }
