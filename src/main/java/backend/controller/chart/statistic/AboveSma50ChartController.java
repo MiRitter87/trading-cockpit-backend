0fbd7ead13cbfd35f64a7a799f031b78c6ced48a
MiRitter87 <michaelritter@gmx.net>
2024-10-09 09:38:29 +0200

    Add SMA(10) to chart showing percentage above SMA(50).

--- src/main/java/backend/controller/chart/statistic/AboveSma50ChartController.java
+++ src/main/java/backend/controller/chart/statistic/AboveSma50ChartController.java
@@ -1,17 +1,26 @@
 package backend.controller.chart.statistic;
 
+import java.awt.Color;
+import java.math.BigDecimal;
 import java.util.ListIterator;
 import java.util.TimeZone;
 
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.JFreeChart;
 import org.jfree.chart.axis.AxisLocation;
+import org.jfree.chart.plot.DatasetRenderingOrder;
+import org.jfree.chart.plot.XYPlot;
+import org.jfree.chart.renderer.xy.XYItemRenderer;
+import org.jfree.chart.renderer.xy.XYLineAndShapeRenderer;
 import org.jfree.data.time.Day;
 import org.jfree.data.time.TimePeriodAnchor;
 import org.jfree.data.time.TimeSeries;
 import org.jfree.data.time.TimeSeriesCollection;
 import org.jfree.data.xy.XYDataset;
 
+import backend.controller.scan.MovingAverageCalculator;
+import backend.model.instrument.Quotation;
+import backend.model.instrument.QuotationArray;
 import backend.model.statistic.Statistic;
 
 /**
@@ -42,6 +51,7 @@ public class AboveSma50ChartController extends StatisticChartController {
         JFreeChart chart = ChartFactory.createTimeSeriesChart(
                 this.getResources().getString("chart.aboveSma50.titleName"), null, null, dataset, true, true, false);
 
+        this.addSma10(chart);
         this.applyBackgroundTheme(chart);
         chart.getXYPlot().setRangeAxisLocation(AxisLocation.TOP_OR_RIGHT);
 
@@ -72,5 +82,91 @@ public class AboveSma50ChartController extends StatisticChartController {
         timeSeriesColleciton.setXPosition(TimePeriodAnchor.MIDDLE);
 
         return timeSeriesColleciton;
+    }
+
+    /**
+     * Adds the SMA(10) to the chart.
+     *
+     * @param chart The chart with the percentage of instruments trading above their SMA(50).
+     */
+    private void addSma10(final JFreeChart chart) {
+        TimeSeriesCollection sma10TimeSeriesCollection;
+        XYPlot plot = (XYPlot) chart.getPlot();
+        XYItemRenderer smaRenderer = new XYLineAndShapeRenderer(true, false);
+        int index = plot.getDatasetCount();
+
+        sma10TimeSeriesCollection = this.getSma10TimeSeriesCollection();
+
+        plot.setDataset(index, sma10TimeSeriesCollection);
+        plot.mapDatasetToRangeAxis(index, 0);
+
+        smaRenderer.setSeriesPaint(0, Color.BLUE);
+
+        plot.setRenderer(index, smaRenderer);
+        plot.setDatasetRenderingOrder(DatasetRenderingOrder.FORWARD);
+    }
+
+    /**
+     * Provides a TimeSeriesCollection with the values of the SMA(10) of the % above SMA(50).
+     *
+     * @return The TimeSeriesCollection with the values of the SMA(10) of the % above SMA(50).
+     */
+    private TimeSeriesCollection getSma10TimeSeriesCollection() {
+        MovingAverageCalculator maCalculator = new MovingAverageCalculator();
+        TimeSeriesCollection timeSeriesCollection = new TimeSeriesCollection();
+        TimeSeries sma10TimeSeries = new TimeSeries(
+                this.getResources().getString("chart.aboveSma50.timeSeriesSma10Name"));
+        QuotationArray quotationArray = this.getPercentAboveSma50AsQuotationArray();
+        ListIterator<Quotation> iterator;
+        Quotation quotation;
+        float sma10;
+        final int days10 = 10;
+        int indexOfQuotation;
+
+        quotationArray.sortQuotationsByDate();
+
+        // Iterate quotations backwards because XYDatasets are constructed from oldest to newest value.
+        iterator = quotationArray.getQuotations().listIterator(quotationArray.getQuotations().size());
+        while (iterator.hasPrevious()) {
+            quotation = iterator.previous();
+            indexOfQuotation = quotationArray.getQuotations().indexOf(quotation);
+
+            // Skip SMA(10) calculation for the oldest 9 days.
+            if ((quotationArray.getQuotations().size() - days10 - indexOfQuotation) < 0) {
+                continue;
+            }
+
+            sma10 = maCalculator.getSimpleMovingAverage(days10, quotation, quotationArray);
+            sma10TimeSeries.add(new Day(quotation.getDate()), sma10);
+        }
+
+        timeSeriesCollection.addSeries(sma10TimeSeries);
+        timeSeriesCollection.setXPosition(TimePeriodAnchor.MIDDLE);
+
+        return timeSeriesCollection;
+    }
+
+    /**
+     * Provides a QuotationArray where the % above SMA(50) is stored in the closing price of each Quotation. This way
+     * the SMA(10) can be calculated easily using existing methods.
+     *
+     * @return A QuotationArray.
+     */
+    private QuotationArray getPercentAboveSma50AsQuotationArray() {
+        Quotation quotation;
+        QuotationArray quotationArray = new QuotationArray();
+        ListIterator<Statistic> iterator;
+        Statistic statistic;
+
+        iterator = this.getStatistics().listIterator(this.getStatistics().size());
+        while (iterator.hasPrevious()) {
+            statistic = iterator.previous();
+            quotation = new Quotation();
+            quotation.setClose(new BigDecimal(statistic.getPercentAboveSma50()));
+            quotation.setDate(statistic.getDate());
+            quotationArray.getQuotations().add(quotation);
+        }
+
+        return quotationArray;
     }
 }
