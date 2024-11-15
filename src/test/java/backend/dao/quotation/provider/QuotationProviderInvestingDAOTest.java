a2dc3c7db0a42bad121a9c16e2e3553c37971e14
MiRitter87 <michaelritter@gmx.net>
2024-11-15 10:22:17 +0100

    Add explorative test parsing investing data from JSON string.

--- src/test/java/backend/dao/quotation/provider/QuotationProviderInvestingDAOTest.java
+++ src/test/java/backend/dao/quotation/provider/QuotationProviderInvestingDAOTest.java
@@ -4,14 +4,25 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.fail;
 
+import java.io.IOException;
 import java.io.InputStream;
 import java.math.BigDecimal;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 import org.apache.commons.io.IOUtils;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import backend.model.Currency;
 import backend.model.StockExchange;
@@ -197,7 +208,7 @@ public class QuotationProviderInvestingDAOTest {
         }
     }
 
-    //@Test
+    // @Test
     /**
      * An explorative test that tries to retrieve Quotation data using a cURL command.
      */
@@ -227,5 +238,81 @@ public class QuotationProviderInvestingDAOTest {
                 process.destroy();
             }
         }
+    }
+
+    // @Test
+    /**
+     * An explorative test that extracts the most recent price from the JSON result.
+     *
+     * This test becomes obsolete, as soon as the functionality moves to distinct methods of the
+     * QuotationProviderInvestingDAO.
+     */
+    public void getCurrentPriceFromJSON() {
+        String jsonPath = "src/test/resources/Investing/investingCurlResultAAPL.json";
+        String quotationDataJSON;
+        Quotation quotation;
+        final Date expectedDate = new Date(1731542400000L);
+        final BigDecimal expectedOpen = new BigDecimal("225.02");
+        final BigDecimal expectedHigh = new BigDecimal("228.87");
+        final BigDecimal expectedLow = new BigDecimal("225");
+        final BigDecimal expectedClose = new BigDecimal("228.22");
+        final long expectedVolume = 44923940;
+
+        try {
+            quotationDataJSON = Files.readString(Paths.get(jsonPath));
+            quotation = this.getQuotationFromJson(quotationDataJSON);
+
+            // Assure Quotation contains the expected data.
+            assertEquals(expectedDate.getTime(), quotation.getDate().getTime());
+            assertEquals(expectedOpen, quotation.getOpen());
+            assertEquals(expectedHigh, quotation.getHigh());
+            assertEquals(expectedLow, quotation.getLow());
+            assertEquals(expectedClose, quotation.getClose());
+            assertEquals(expectedVolume, quotation.getVolume());
+        } catch (IOException e) {
+            fail(e.getMessage());
+        }
+    }
+
+    /**
+     * Gets a Quotation from the given JSON String with quotation data.
+     *
+     * @param jsonString A JSON string containing multiple quotations.
+     * @return The newest Quotation.
+     * @throws JsonMappingException    JSON Mapping failed.
+     * @throws JsonProcessingException JSON processing failed.
+     */
+    @SuppressWarnings("unchecked")
+    private Quotation getQuotationFromJson(final String jsonString)
+            throws JsonMappingException, JsonProcessingException {
+        String price;
+        String volume;
+        ObjectMapper mapper = new ObjectMapper();
+        Map<?, ?> map;
+        ArrayList<ArrayList<Object>> quotations;
+        ArrayList<Object> mostRecentQuotation;
+        Quotation quotation = new Quotation();
+
+        // Get the data of the most recent Quotation.
+        map = mapper.readValue(jsonString, Map.class);
+        quotations = (ArrayList<ArrayList<Object>>) map.get("data");
+        mostRecentQuotation = quotations.get(quotations.size() - 1);
+
+        // Convert raw data to Quotation object.
+        quotation.setDate(new Date((long) mostRecentQuotation.get(0)));
+
+        price = mostRecentQuotation.get(1).toString();
+        quotation.setOpen(new BigDecimal(price));
+        price = mostRecentQuotation.get(2).toString();
+        quotation.setHigh(new BigDecimal(price));
+        price = mostRecentQuotation.get(3).toString();
+        quotation.setLow(new BigDecimal(price));
+        price = mostRecentQuotation.get(4).toString();
+        quotation.setClose(new BigDecimal(price));
+
+        volume = mostRecentQuotation.get(5).toString();
+        quotation.setVolume(Long.parseLong(volume));
+
+        return quotation;
     }
 }
