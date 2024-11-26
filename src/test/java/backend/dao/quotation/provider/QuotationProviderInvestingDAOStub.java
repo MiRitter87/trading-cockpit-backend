36ea585339a13616c6b7597196481bddfee1378b
MiRitter87 <michaelritter@gmx.net>
2024-11-26 10:20:19 +0100

    Prepare InvestingDAOStub for loading of history using JSON file.

--- src/test/java/backend/dao/quotation/provider/QuotationProviderInvestingDAOStub.java
+++ src/test/java/backend/dao/quotation/provider/QuotationProviderInvestingDAOStub.java
@@ -2,6 +2,7 @@ package backend.dao.quotation.provider;
 
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.List;
 
 import backend.model.StockExchange;
 import backend.model.instrument.Instrument;
@@ -28,5 +29,22 @@ public class QuotationProviderInvestingDAOStub extends QuotationProviderInvestin
         quotationHistoryJSON = Files.readString(Paths.get(jsonPath));
 
         return this.convertJSONtoCurrentQuotation(quotationHistoryJSON, instrument);
+    }
+
+    @Override
+    public List<Quotation> getQuotationHistory(Instrument instrument, Integer years) throws Exception {
+        String jsonPath = "";
+        String quotationHistoryJSON;
+
+        if (instrument.getSymbol().equals("DML") && instrument.getStockExchange().equals(StockExchange.TSX)) {
+            jsonPath = "src/test/resources/Investing/investingTSXHistoryDML.json";
+        } else {
+            return null;
+        }
+
+        quotationHistoryJSON = Files.readString(Paths.get(jsonPath));
+
+        // return this.convertJSONToQuotations(quotationHistoryJson, instrument.getStockExchange());
+        return null;
     }
 }
