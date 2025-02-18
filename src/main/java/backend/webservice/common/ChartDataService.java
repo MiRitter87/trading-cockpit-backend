43fc955a2c743cf3840eb032ee152e02625ce15c
MiRitter87 <michaelritter@gmx.net>
2025-02-18 09:13:45 +0100

    Implement method 'getPriceVolumeData' in ChartDataService.

--- src/main/java/backend/webservice/common/ChartDataService.java
+++ src/main/java/backend/webservice/common/ChartDataService.java
@@ -1,5 +1,15 @@
 package backend.webservice.common;
 
+import java.text.MessageFormat;
+import java.util.ResourceBundle;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import backend.controller.chart.data.PriceVolumeDataController;
+import backend.model.instrument.QuotationArray;
+import backend.model.webservice.WebServiceMessage;
+import backend.model.webservice.WebServiceMessageType;
 import backend.model.webservice.WebServiceResult;
 
 /**
@@ -9,12 +19,35 @@ import backend.model.webservice.WebServiceResult;
  */
 public class ChartDataService {
     /**
+     * Access to localized application resources.
+     */
+    private ResourceBundle resources = ResourceBundle.getBundle("backend");
+
+    /**
+     * Application logging.
+     */
+    public static final Logger LOGGER = LogManager.getLogger(ChartDataService.class);
+
+    /**
      * Provides data to build a price/volume chart of an Instrument.
      *
-     * @param instrumentId   The ID of the Instrument whose data are requested.
+     * @param instrumentId The ID of the Instrument whose data are requested.
      * @return The chart data.
      */
     public WebServiceResult getPriceVolumeData(final Integer instrumentId) {
-        return null;
+        PriceVolumeDataController priceVolumeDataController = new PriceVolumeDataController();
+        WebServiceResult getPriceVolumeDataResult = new WebServiceResult(null);
+        QuotationArray quotations = new QuotationArray();
+
+        try {
+            quotations.setQuotations(priceVolumeDataController.getPriceVolumeData(instrumentId));
+            getPriceVolumeDataResult.setData(quotations);
+        } catch (Exception e) {
+            getPriceVolumeDataResult.addMessage(new WebServiceMessage(WebServiceMessageType.E,
+                    this.resources.getString("chartData.priceVolume.getError")));
+            LOGGER.error(MessageFormat.format(this.resources.getString("chartData.priceVolume.getError"), e));
+        }
+
+        return getPriceVolumeDataResult;
     }
 }
