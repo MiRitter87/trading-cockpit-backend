3cf3e2ed94bfc0b24731c9e2b9ed1eae39120991
MiRitter87 <michaelritter@gmx.net>
2026-01-29 08:23:19 +0100

    Change Buyable Base extension criterion from fixed percentage to ATRP.

--- src/main/java/backend/dao/quotation/persistence/TemplateBuyableBaseProcessor.java
+++ src/main/java/backend/dao/quotation/persistence/TemplateBuyableBaseProcessor.java
@@ -13,8 +13,8 @@ import backend.model.instrument.Quotation;
 public class TemplateBuyableBaseProcessor {
     /**
      * Performs post processing tasks for the given quotations based on the ScanTemplate "BUYABLE_BASE". The method
-     * checks for each Instrument of the given quotations if the price is at least 10% above the EMA(21). Extended
-     * quotations are removed from the List of quotations.
+     * checks for each Instrument of the given quotations if the price is at least 1,5 * ATRP(20) above the EMA(21).
+     * Extended quotations are removed from the List of quotations.
      *
      * @param quotations The quotations on which the post processing is performed.
      */
@@ -32,13 +32,16 @@ public class TemplateBuyableBaseProcessor {
     }
 
     /**
-     * Checks if the given quotation is extended by at least 10% above the EMA(21).
+     * Checks if the given quotation is extended by at least 1,5 * ATRP(20) above the EMA(21).
      *
      * @param quotation The Quotation to be checked.
      * @return true, if Quotation is extended above EMA(21).
      */
     private boolean isExtendedAboveEma21(final Quotation quotation) {
-        final float extendedFactor = (float) 1.1;
+        final int hundredPercent = 100;
+        final float atrpFactor = 1.5f;
+        final float extendedFactor = 1
+                + (quotation.getIndicator().getAverageTrueRangePercent20() * atrpFactor / hundredPercent);
         float extendedPrice = quotation.getMovingAverageData().getEma21() * extendedFactor;
 
         if (quotation.getClose().floatValue() >= extendedPrice) {
