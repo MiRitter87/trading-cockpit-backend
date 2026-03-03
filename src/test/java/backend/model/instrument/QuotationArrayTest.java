c61e8fa4d261d26bf0f0be49965606e2d6838a89
MiRitter87 <michaelritter@gmx.net>
2026-03-03 09:33:56 +0100

    Fix checkstyle errors in QuotationArrayTest.

--- src/test/java/backend/model/instrument/QuotationArrayTest.java
+++ src/test/java/backend/model/instrument/QuotationArrayTest.java
@@ -39,10 +39,10 @@ public class QuotationArrayTest {
      */
     private static QuotationProviderDAO quotationProviderYahooDAO;
 
-    @BeforeAll
     /**
      * Tasks to be performed once at startup of test class.
      */
+    @BeforeAll
     public static void setUpClass() {
         try {
             quotationProviderYahooDAO = new QuotationProviderYahooDAOStub();
@@ -51,18 +51,18 @@ public class QuotationArrayTest {
         }
     }
 
-    @AfterAll
     /**
      * Tasks to be performed once at the end of the test class.
      */
+    @AfterAll
     public static void tearDownClass() {
         quotationProviderYahooDAO = null;
     }
 
-    @BeforeEach
     /**
      * Tasks to be performed before each test is run.
      */
+    @BeforeEach
     public void setUp() {
         List<Quotation> quotations = new ArrayList<>();
         Instrument dmlStock = new Instrument();
@@ -79,23 +79,87 @@ public class QuotationArrayTest {
         }
     }
 
-    @AfterEach
     /**
      * Tasks to be performed after each test has been run.
      */
+    @AfterEach
     public void tearDown() {
         this.quotationArray = null;
     }
 
-    @Test
+    /**
+     * Gets the expected weekly Quotation 1.
+     *
+     * @return Quotation 1.
+     */
+    private Quotation getQuotation1() {
+        Quotation quotation1 = new Quotation();
+        final long volume1 = 8146700;
+        final int index4 = 4;
+
+        quotation1.setDate(this.quotationArray.getQuotations().get(index4).getDate());
+        quotation1.setCurrency(Currency.CAD);
+        quotation1.setVolume(volume1);
+        quotation1.setOpen(new BigDecimal("1.38"));
+        quotation1.setHigh(new BigDecimal("1.54"));
+        quotation1.setLow(new BigDecimal("1.35"));
+        quotation1.setClose(new BigDecimal("1.36"));
+
+        return quotation1;
+    }
+
+    /**
+     * Gets the expected weekly Quotation 2.
+     *
+     * @return Quotation 2.
+     */
+    private Quotation getQuotation2() {
+        Quotation quotation2 = new Quotation();
+        final long volume2 = 6258300;
+        final int index9 = 9;
+
+        quotation2.setDate(this.quotationArray.getQuotations().get(index9).getDate());
+        quotation2.setCurrency(Currency.CAD);
+        quotation2.setVolume(volume2);
+        quotation2.setOpen(new BigDecimal("1.34"));
+        quotation2.setHigh(new BigDecimal("1.38"));
+        quotation2.setLow(new BigDecimal("1.24"));
+        quotation2.setClose(new BigDecimal("1.36"));
+
+        return quotation2;
+    }
+
+    /**
+     * Gets the expected weekly Quotation 53.
+     *
+     * @return Quotation 53.
+     */
+    private Quotation getQuotation53() {
+        Quotation quotation53 = new Quotation();
+        final long volume53 = 1886800;
+        final int index251 = 251;
+
+        quotation53.setDate(this.quotationArray.getQuotations().get(index251).getDate());
+        quotation53.setCurrency(Currency.CAD);
+        quotation53.setVolume(volume53);
+        quotation53.setOpen(new BigDecimal("1.35"));
+        quotation53.setHigh(new BigDecimal("1.35"));
+        quotation53.setLow(new BigDecimal("1.27"));
+        quotation53.setClose(new BigDecimal("1.29"));
+
+        return quotation53;
+    }
+
     /**
      * Tests the determination of the age of the newest Quotation within the QuotationArray.
      */
+    @Test
     public void testGetAgeOfNewestQuotationInDays() {
         Date currentDate = new Date();
         LocalDate currentDateLocal = LocalDate.ofInstant(currentDate.toInstant(), ZoneId.systemDefault());
         LocalDate newestQuotationDateLocal;
-        long actualNumberDays, expectedNumberDays;
+        long actualNumberDays;
+        long expectedNumberDays;
         Quotation newestQuotation;
 
         this.quotationArray.sortQuotationsByDate();
@@ -109,14 +173,17 @@ public class QuotationArrayTest {
         assertEquals(expectedNumberDays, actualNumberDays);
     }
 
-    @Test
     /**
      * Tests the retrieval of quotations that are older than the given Quotation but still on the same day.
      */
+    @Test
     public void testGetOlderQuotationsOfSameDay() {
         List<Quotation> olderQuotationsSameDay;
-        Quotation olderQuotationOfSameDay, addedQuotation, quotation;
+        Quotation olderQuotationOfSameDay;
+        Quotation addedQuotation;
+        Quotation quotation;
         Calendar calendar = Calendar.getInstance();
+        final long volume = 150000;
 
         // The newest Quotation of a day.
         quotation = this.quotationArray.getQuotations().get(0);
@@ -127,11 +194,11 @@ public class QuotationArrayTest {
         addedQuotation = new Quotation();
         addedQuotation.setDate(calendar.getTime());
         addedQuotation.setCurrency(quotation.getCurrency());
-        addedQuotation.setVolume(150000);
-        addedQuotation.setOpen(new BigDecimal(1.95));
-        addedQuotation.setHigh(new BigDecimal(2.05));
-        addedQuotation.setLow(new BigDecimal(1.95));
-        addedQuotation.setClose(new BigDecimal(2.00));
+        addedQuotation.setVolume(volume);
+        addedQuotation.setOpen(new BigDecimal("1.95"));
+        addedQuotation.setHigh(new BigDecimal("2.05"));
+        addedQuotation.setLow(new BigDecimal("1.95"));
+        addedQuotation.setClose(new BigDecimal("2.00"));
         this.quotationArray.getQuotations().add(addedQuotation);
 
         olderQuotationsSameDay = this.quotationArray.getOlderQuotationsOfSameDay(quotation.getDate());
@@ -142,15 +209,16 @@ public class QuotationArrayTest {
         assertEquals(addedQuotation, olderQuotationOfSameDay);
     }
 
-    @Test
     /**
      * Tests the retrieval of the newest Quotation of the given date.
      */
+    @Test
     public void testGetNewestQuotationOfDate() {
         Quotation expectedQuotation;
         Quotation actualQuotation;
         Quotation addedQuotation;
         Calendar calendar = Calendar.getInstance();
+        final long volume = 150000;
 
         // The newest Quotation of a day.
         expectedQuotation = this.quotationArray.getQuotations().get(0);
@@ -161,11 +229,11 @@ public class QuotationArrayTest {
         addedQuotation = new Quotation();
         addedQuotation.setDate(calendar.getTime());
         addedQuotation.setCurrency(expectedQuotation.getCurrency());
-        addedQuotation.setVolume(150000);
-        addedQuotation.setOpen(new BigDecimal(1.95));
-        addedQuotation.setHigh(new BigDecimal(2.05));
-        addedQuotation.setLow(new BigDecimal(1.95));
-        addedQuotation.setClose(new BigDecimal(2.00));
+        addedQuotation.setVolume(volume);
+        addedQuotation.setOpen(new BigDecimal("1.95"));
+        addedQuotation.setHigh(new BigDecimal("2.05"));
+        addedQuotation.setLow(new BigDecimal("1.95"));
+        addedQuotation.setClose(new BigDecimal("2.00"));
         this.quotationArray.getQuotations().add(addedQuotation);
 
         actualQuotation = this.quotationArray.getNewestQuotationOfDate(addedQuotation.getDate());
@@ -173,73 +241,47 @@ public class QuotationArrayTest {
         assertEquals(expectedQuotation, actualQuotation);
     }
 
-    @Test
     /**
      * Tests the determination of weekly quotations.
      */
+    @Test
     public void testGetWeeklyQuotations() {
-        Quotation quotation1;
-        Quotation quotation2;
-        Quotation quotation53;
+        Quotation expectedQuotation1 = this.getQuotation1();
+        Quotation expectedQuotation2 = this.getQuotation2();
+        Quotation expectedQuotation53 = this.getQuotation53();
         Quotation currentQuotation;
         List<Quotation> weeklyQuotations;
-
-        // Define the most recent two of the expected weekly quotations.
-        quotation1 = new Quotation();
-        quotation1.setDate(this.quotationArray.getQuotations().get(4).getDate());
-        quotation1.setCurrency(Currency.CAD);
-        quotation1.setVolume(8146700);
-        quotation1.setOpen(new BigDecimal("1.38"));
-        quotation1.setHigh(new BigDecimal("1.54"));
-        quotation1.setLow(new BigDecimal("1.35"));
-        quotation1.setClose(new BigDecimal("1.36"));
-
-        quotation2 = new Quotation();
-        quotation2.setDate(this.quotationArray.getQuotations().get(9).getDate());
-        quotation2.setCurrency(Currency.CAD);
-        quotation2.setVolume(6258300);
-        quotation2.setOpen(new BigDecimal("1.34"));
-        quotation2.setHigh(new BigDecimal("1.38"));
-        quotation2.setLow(new BigDecimal("1.24"));
-        quotation2.setClose(new BigDecimal("1.36"));
-
-        // Define the last expected weekly Quotation.
-        quotation53 = new Quotation();
-        quotation53.setDate(this.quotationArray.getQuotations().get(251).getDate());
-        quotation53.setCurrency(Currency.CAD);
-        quotation53.setVolume(1886800);
-        quotation53.setOpen(new BigDecimal("1.35"));
-        quotation53.setHigh(new BigDecimal("1.35"));
-        quotation53.setLow(new BigDecimal("1.27"));
-        quotation53.setClose(new BigDecimal("1.29"));
+        final int week53 = 53;
+        final int index52 = 52;
 
         weeklyQuotations = this.quotationArray.getWeeklyQuotations(null);
 
         // Assure that there is one Quotation for each week of the year.
         // The last two days are part of week 53.
-        assertEquals(53, weeklyQuotations.size());
+        assertEquals(week53, weeklyQuotations.size());
 
         // Check the newest two weekly quotations.
         currentQuotation = weeklyQuotations.get(0);
-        assertEquals(quotation1, currentQuotation);
+        assertEquals(expectedQuotation1, currentQuotation);
 
         currentQuotation = weeklyQuotations.get(1);
-        assertEquals(quotation2, currentQuotation);
+        assertEquals(expectedQuotation2, currentQuotation);
 
         // Check the oldest weekly Quotation.
-        currentQuotation = weeklyQuotations.get(52);
-        assertEquals(quotation53, currentQuotation);
+        currentQuotation = weeklyQuotations.get(index52);
+        assertEquals(expectedQuotation53, currentQuotation);
     }
 
-    @Test
     /**
      * Tests the determination of weekly quotations if only quotations of two days exist.
      */
+    @Test
     public void testGetWeeklyQuotations2Days() {
         QuotationArray twoDaysQuotationArray = new QuotationArray();
         List<Quotation> weeklyQuotations;
         Quotation currentQuotation;
         Quotation expectedQuotation;
+        final long volume = 3244200;
 
         // Initialize QuotationArray with quotations of two days.
         twoDaysQuotationArray.getQuotations().add(this.quotationArray.getQuotations().get(0));
@@ -249,7 +291,7 @@ public class QuotationArrayTest {
         expectedQuotation = new Quotation();
         expectedQuotation.setDate(this.quotationArray.getQuotations().get(1).getDate());
         expectedQuotation.setCurrency(Currency.CAD);
-        expectedQuotation.setVolume(3244200);
+        expectedQuotation.setVolume(volume);
         expectedQuotation.setOpen(new BigDecimal("1.50"));
         expectedQuotation.setHigh(new BigDecimal("1.52"));
         expectedQuotation.setLow(new BigDecimal("1.35"));
@@ -265,22 +307,25 @@ public class QuotationArrayTest {
         assertEquals(expectedQuotation, currentQuotation);
     }
 
-    @Test
     /**
      * Tests the determination of weekly quotations. The newest daily Quotation is not taken into account.
      */
+    @Test
     public void testGetWeeklyQuotationsSkipNewestDay() {
         Quotation actualQuotation;
         Quotation expectedQuotation;
         List<Quotation> weeklyQuotations;
+        final long volume = 6353400;
+        final int index4 = 4;
+        final int expectedWeeks = 53;
 
         this.quotationArray.sortQuotationsByDate();
 
         // Define the expected weekly Quotation.
         expectedQuotation = new Quotation();
-        expectedQuotation.setDate(this.quotationArray.getQuotations().get(4).getDate());
+        expectedQuotation.setDate(this.quotationArray.getQuotations().get(index4).getDate());
         expectedQuotation.setCurrency(Currency.CAD);
-        expectedQuotation.setVolume(6353400);
+        expectedQuotation.setVolume(volume);
         expectedQuotation.setOpen(new BigDecimal("1.38"));
         expectedQuotation.setHigh(new BigDecimal("1.54"));
         expectedQuotation.setLow(new BigDecimal("1.38"));
@@ -290,7 +335,7 @@ public class QuotationArrayTest {
 
         // Assure that there is one Quotation for each week of the year.
         // The last two days are part of week 53.
-        assertEquals(53, weeklyQuotations.size());
+        assertEquals(expectedWeeks, weeklyQuotations.size());
 
         // Check the newest weekly quotation.
         actualQuotation = weeklyQuotations.get(0);
